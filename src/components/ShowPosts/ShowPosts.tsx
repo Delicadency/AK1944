4c9026d9ae0f474065beff131f89e9f27f9d1c8f
PiotrDrechsler <pdrech@gmail.com>
2024-11-29 20:40:30 +0100

    Refactor: Improve data fetching hook and error handling
    - Updated the `useFetchData` hook to improve readability and error handling.
    - Added better error messages in case of failed data fetching.
    - Simplified the `ShowPosts` component by handling pending and error states with conditional rendering.
    - Now, the hook returns `isPending`, `data`, and `error` to manage the data fetching state more clearly.

--- src/components/ShowPosts/ShowPosts.tsx
+++ src/components/ShowPosts/ShowPosts.tsx
@@ -4,22 +4,21 @@ import { useFetchData } from "@/hooks/useFetchData";
 import parse from "html-react-parser";
 
 export const ShowPosts = () => {
-  const { data: posts, isPending } = useFetchData(
+  const { data, isPending, error } = useFetchData(
     "https://wordpressapi.eu/wp-json/wp/v2/posts/",
   );
 
   return (
     <>
-      {isPending ? (
-        <div>Wczytuje dane...</div>
-      ) : (
-        posts.map((post) => (
+      {isPending && <div>Wczytuje dane....</div>}
+      {error && <div>{error}</div>}
+      {data &&
+        data.map((post) => (
           <div key={post.id}>
             <h2 className="text-2xl">{post.title.rendered}</h2>
             <div className="text-lg">{parse(post.content.rendered)}</div>
           </div>
-        ))
-      )}
+        ))}
     </>
   );
 };
--- src/hooks/useFetchData.ts
+++ src/hooks/useFetchData.ts
@@ -13,16 +13,19 @@ interface Post {
 export const useFetchData = (url: string) => {
   const [isPending, setIsPending] = useState(true);
   const [data, setData] = useState<Post[]>([]);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
+      setIsPending(true);
       try {
-        setIsPending(true);
-        const res = await fetch(url);
-        const result: Post[] = await res.json();
-        setData(result);
-      } catch (e) {
-        console.log("Error fetching data", e);
+        const response = await fetch(url);
+        if (!response.ok) throw new Error(response.statusText);
+        const json: Post[] = await response.json();
+        setData(json);
+        setError(null);
+      } catch (error) {
+        setError(`${error} Nie udało się pobrać danych`);
       } finally {
         setIsPending(false);
       }
@@ -31,5 +34,5 @@ export const useFetchData = (url: string) => {
     fetchData();
   }, [url, setIsPending, setData]);
 
-  return { data, isPending };
+  return { data, isPending, error };
 };
