841685496394729ee366a1f6a93d425c7e14d756
Agata Kamińska <delicadency@gmail.com>
2024-11-30 16:19:56 +0100

    fix: test environment configuration improved

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,3 @@
-import "@testing-library/jest-dom/extend-expect";
 import "@testing-library/jest-dom";
 
 // Do uzupełnienia mockowanie wybranych elementów next.js wykorzystywanych w testach, na razie zostawiam puste
