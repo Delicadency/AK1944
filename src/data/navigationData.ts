48f5bbd5e598875cb37ad293f760631db9939560
Agata Kamińska <delicadency@gmail.com>
2024-12-13 15:27:15 +0100

    feat: created navigation data

---
+++ src/data/navigationData.ts
@@ -0,0 +1,42 @@
+export const navData = [
+  { href: "/", label: "Start" },
+  { href: "", label: "Światowy Związek Żołnierzy AK" },
+  { href: "/gallery", label: "Galeria" },
+  { href: "/partners", label: "Partnerzy" },
+  { href: "/contact", label: "Kontakt" },
+  { href: "", label: "Historia" },
+  { href: "", label: "Szlak Partyzancki" },
+  { href: "/graves", label: "Inwentarz grobów" },
+  { href: "/biograms", label: "Biogramy" },
+];
+
+export const homeArmyUnionNavData = [
+  { href: "/about", label: "O nas" },
+  { href: "/board", label: "Zarząd" },
+  { href: "/union", label: "Historia Związku" },
+  { href: "/calendar", label: "Kalendarz uroczystości" },
+  { href: "/projects", label: "Realizowane projekty" },
+  { href: "/memorials", label: "Miejsca pamięci" },
+];
+
+export const historyNavData = [
+  { href: "/5th-regiment-home-army", label: "5. Pułk Strzelców Konnych" },
+  { href: "/debica-district-szp-zwz-ak", label: "Obwód Dębica SZP-ZWZ-AK" },
+  { href: "/operation-burza", label: "Operacja Burza" },
+  { href: "/literature", label: "Literatura" },
+];
+
+export const partisanTrailData = [
+  { href: "/partisan-trail", label: "Szlak Partyzancki" },
+  { href: "/origins", label: "Geneza" },
+  { href: "/rallies", label: "Rajdy" },
+];
+
+export const footerNavData = [
+  { href: "/about", label: "O nas" },
+  { href: "/partisan-trail", label: "Szlak Partyzancki" },
+  { href: "/graves", label: "Inwentarz grobów" },
+  { href: "/partners", label: "Partnerzy" },
+  { href: "/contact", label: "Kontakt" },
+  { href: "/support", label: "Wesprzyj" },
+];
