259fa1a52518b044798f55881a4ae70fb70b9e5f
Agata Kamińska <delicadency@gmail.com>
2024-12-19 19:27:35 +0100

    feat: changed button to Next Link element; added interface props

--- src/components/shared/Button/Button.tsx
+++ src/components/shared/Button/Button.tsx
@@ -1,26 +1,37 @@
 import clsx from "clsx";
+import Link from "next/link";
 
 export interface ButtonProps {
   variant: "primary" | "inversion" | "secondary";
+  size: "medium" | "large";
+  icon: boolean;
   label: string;
+  href: string;
 }
 
-export const Button = ({ variant, label }: ButtonProps) => {
+export const Button = ({ variant, label, size, href }: ButtonProps) => {
   const variants = {
-    primary: "bg-redMain text-backgroundMain hover:bg-[#7A0003] active:bg-[#520103]",
-    inversion: "bg-backgroundMain text-redMain hover:bg-backgroundB active:bg-[#e8e3da]",
+    primary:
+      "bg-redMain text-backgroundMain hover:bg-[#7A0003] active:bg-[#520103]",
+    inversion:
+      "bg-backgroundMain text-redMain hover:bg-backgroundB active:bg-[#e8e3da]",
     secondary: "bg-greenLight text-black hover:bg-greenC active:bg-greenB",
   };
-
+  //active and hover are not set in Figma yet, to change later !!!
+  const sizes = {
+    medium: "text-16",
+    large: "text-lg",
+  };
   return (
-    <button
-    type="button"
+    <Link
+      href={href}
       className={clsx(
-        "font-sourceSans button-outline h-fit w-[160px] rounded px-8 py-3 text-lg",
+        "button-outline h-fit rounded px-8 py-3 font-sourceSans shadow-inner",
         variants[variant],
+        sizes[size],
       )}
     >
       {label}
-    </button>
+    </Link>
   );
 };
