5e2d9bced6c9a52dc8aff93495a4143e9bfcd8c2
Jakub Pawlak <82868@student.pb.edu.pl>
2024-12-23 19:15:25 +0100

    refactor: use cn() utility function for class name merging in layout, ExampleButton, and Container components

--- src/components/ExampleButton/ExampleButton.tsx
+++ src/components/ExampleButton/ExampleButton.tsx
@@ -1,4 +1,4 @@
-import clsx from "clsx";
+import { cn } from "@/utils";
 
 export interface ExampleButtonProps {
   variant: "primary" | "inversion" | "secondary";
@@ -7,16 +7,18 @@ export interface ExampleButtonProps {
 
 export const ExampleButton = ({ variant, label }: ExampleButtonProps) => {
   const variants = {
-    primary: "bg-redMain text-backgroundMain hover:bg-[#7A0003] active:bg-[#520103]",
-    inversion: "bg-backgroundMain text-redMain hover:bg-backgroundB active:bg-[#e8e3da]",
+    primary:
+      "bg-redMain text-backgroundMain hover:bg-[#7A0003] active:bg-[#520103]",
+    inversion:
+      "bg-backgroundMain text-redMain hover:bg-backgroundB active:bg-[#e8e3da]",
     secondary: "bg-greenLight text-black hover:bg-greenC active:bg-greenB",
   };
 
   return (
     <button
-    type="button"
-      className={clsx(
-        "font-sourceSans button-outline h-fit w-[160px] rounded px-8 py-3 text-lg",
+      type="button"
+      className={cn(
+        "button-outline h-fit w-[160px] rounded px-8 py-3 font-sourceSans text-lg",
         variants[variant],
       )}
     >
