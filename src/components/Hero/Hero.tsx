bafc4f1263798f4503ad9b786b269d713ea9fa0e
Okazaki92 <vincentslominski@gmail.com>
2025-01-11 20:58:52 +0100

    feat/ initial commit, created Hero component

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,24 @@
+import { BackgroundImage } from "../shared/BackgroundImage/BackgroundImage";
+import Container from "../shared/Container";
+import { Heading } from "../shared/Heading/Heading";
+
+const Hero = () => {
+  return (
+    <BackgroundImage
+      alt="background image"
+      src="/images/hero_background.png"
+      width={1920}
+      height={1080}
+    >
+      <Container>
+        <Heading variant="h1" contrast="yellow" color="white" underline>
+          Armia Krajowa Dębica
+        </Heading>
+        <Heading variant="h3" contrast="yellow" color="white">
+          Pamięć o Nas nie zginie
+        </Heading>
+      </Container>
+    </BackgroundImage>
+  );
+};
+export default Hero;
