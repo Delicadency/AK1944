0b7d219ebbc6e1e1180a78beb70d6622d6298d44
Agata Kamińska <delicadency@gmail.com>
2025-01-12 16:24:04 +0100

    feat: header structure added

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,31 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import Container from "../shared/Container";
 import { HeaderMobile } from "./HeaderMobile";
 
 export default function Header() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkScreenSize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+    checkScreenSize();
+    window.addEventListener("resize", checkScreenSize);
+    return () => window.removeEventListener("resize", checkScreenSize);
+  }, []);
+
   return (
     <header className="fixed h-[80px] w-full bg-greenMain contrast:bg-yellow tablet:h-[148px] desktop:h-[140px]">
       <Container>
-        <div className="">
-          <HeaderMobile />
-        </div>
+        {isMobile ? (
+          <div className="">
+            <HeaderMobile />
+          </div>
+        ) : (
+          <div></div>
+        )}
       </Container>
     </header>
   );
