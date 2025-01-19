c56f89e765abf16906ee047dd0bd93782f3e0f3c
PiotrDrechsler <pdrech@gmail.com>
2025-01-19 19:40:52 +0100

    vercel fix
    delete unsued data in storybook

--- src/components/shared/NewsItem/NewsItem.stories.tsx
+++ src/components/shared/NewsItem/NewsItem.stories.tsx
@@ -15,12 +15,8 @@ const meta: Meta<typeof NewsItem> = {
         type: "text",
       },
     },
-    image: {
-      control: {
-        type: "object",
-      },
-    },
-    text: {
+
+    excerpt: {
       control: {
         type: "text",
       },
@@ -41,12 +37,7 @@ export const Default: Story = {
   args: {
     heading: "Lorem ipsum",
     date: "12.12.2024",
-    image: {
-      src: "/images/news_placeholder.png",
-      alt: "news image",
-      width: 400,
-      height: 300,
-    },
-    text: "Lorem ipsum dolor sit amet consectetur. Imperdiet est libero faucibus sed nullam nibh tempus massa ipsum. Vel commodo urna praesent justo convallis vel duis justo. Placerat ut egestas volutpat tincidunt sed. Diam pharetra sed ultricies mi eget mauris pharetra et ultrices. Eget nunc scelerisque viverra ",
+    excerpt:
+      "Lorem ipsum dolor sit amet consectetur. Imperdiet est libero faucibus sed nullam nibh tempus massa ipsum. Vel commodo urna praesent justo convallis vel duis justo. Placerat ut egestas volutpat tincidunt sed. Diam pharetra sed ultricies mi eget mauris pharetra et ultrices. Eget nunc scelerisque viverra ",
   },
 };
