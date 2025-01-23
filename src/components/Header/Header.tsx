9574cfa09d0e816638afab27832a1b1249dfa5ef
Agata Kamińska <delicadency@gmail.com>
2025-01-23 18:30:33 +0100

    feat: custom hook useMediaQuery added; refactor: simplyfied the logic of Header component by using useMediaQuery hook;

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,23 +1,14 @@
 "use client";
-import { useEffect, useState } from "react";
 import Container from "../shared/Container";
 import { HeaderMobile } from "./HeaderMobile";
 import { HeaderTabAndDesktop } from "./HeaderTabAndDesktop";
+import { useMediaQuery } from "@/hooks/useMediaQuery";
 
 export default function Header() {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    const checkScreenSize = () => {
-      setIsMobile(window.innerWidth < 768);
-    };
-    checkScreenSize();
-    window.addEventListener("resize", checkScreenSize);
-    return () => window.removeEventListener("resize", checkScreenSize);
-  }, []);
+  const isMobile = useMediaQuery("(max-width: 767px)");
 
   return (
-    <header className="contrast:bg-yellowContrast z-20 fixed h-[80px] w-full bg-greenMain tablet:h-[148px] desktop:h-[140px]">
+    <header className="fixed z-20 h-[80px] w-full bg-greenMain contrast:bg-yellowContrast tablet:h-[148px] desktop:h-[140px]">
       <Container>
         {isMobile ? <HeaderMobile /> : <HeaderTabAndDesktop />}
       </Container>
