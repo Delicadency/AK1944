326f442eaecfa2260762f21f79cfe32c56e40758
Okazaki92 <vincentslominski@gmail.com>
2025-01-25 18:03:28 +0100

    feat: implement AboutUsLandingCard component and refactor AboutUsLanding

--- src/components/AboutUsLanding/AboutUsLanding.tsx
+++ src/components/AboutUsLanding/AboutUsLanding.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import Image from "next/image";
 import { Button } from "../shared/Button/Button";
 import Container from "../shared/Container";
 import { Heading } from "../shared/Heading/Heading";
+import AboutUsLandingCard from "../shared/AboutUsLandingCard/AboutUsLandingCard";
 
 const AboutUsLanding = () => {
   return (
@@ -28,78 +28,41 @@ const AboutUsLanding = () => {
         />
       </Container>
       <div className="flex w-full flex-col gap-10 tablet:snap-mandatory tablet:flex-row tablet:overflow-x-auto tablet:scroll-smooth tablet:px-11 desktop:mx-auto desktop:max-w-[1440px] desktop:items-center desktop:justify-center desktop:gap-6 desktop:overflow-x-hidden desktop:px-28">
-        <Container className="flex w-[320px] flex-col items-center gap-6 rounded bg-greenMain py-5 contrast:bg-yellowContrast tablet:w-[395px] tablet:flex-shrink-0 tablet:snap-start tablet:gap-3 tablet:px-6 desktop:snap-none">
-          <Image
-            src="/images/history_AK.png"
-            alt="Historia AK"
-            width={350}
-            height={190}
-            className="h-[191px] w-[288px] rounded object-cover tablet:mb-5 tablet:w-[350px]"
-          />
-          <div className="self-start">
-            <Heading color="white" contrast="black" variant="h3">
-              Historia AK
-            </Heading>
-          </div>
-          <p className="text-white contrast:text-black tablet:mb-1">
-            ŚZŻAK powstał w 1990 roku w Polsce z inicjatywy byłych żołnierzy
+        <AboutUsLandingCard
+          heading="Historia AK"
+          description="ŚZŻAK powstał w 1990 roku w Polsce z inicjatywy byłych żołnierzy
             Armii Krajowej, którzy pragnęli upamiętnić działalność jednej z
-            największych podziemnych armii II wojny światowej.
-          </p>
-          <Button
-            label="Poznaj historię"
-            ariaDescription="Poznaj historię"
-            variant="secondary"
-          />
-        </Container>
-        <Container className="flex w-[320px] flex-col items-center gap-6 rounded bg-greenMain py-5 contrast:bg-yellowContrast tablet:w-[395px] tablet:flex-shrink-0 tablet:snap-start tablet:gap-3 tablet:px-6 desktop:snap-none">
-          <Image
-            src="/images/partisan_trail.png"
-            alt="Szlak Partyzancki"
-            width={350}
-            height={190}
-            className="h-[191px] w-[288px] rounded object-cover tablet:mb-5 tablet:w-[350px]"
-          />
-          <div className="self-start">
-            <Heading color="white" contrast="black" variant="h3">
-              Szlak Partyzancki
-            </Heading>
-          </div>
-          <p className="text-white contrast:text-black tablet:mb-1">
-            Jest propozycją dla Wszystkich rozmiłowanych w pieszych wędrówkach i
-            historii oraz cenną pomocą dydaktyczną dla nauczycieli i rodziców w
-            wychowaniu patriotycznym dzieci i młodzieży.
-          </p>
-          <Button
-            label="Czytaj więcej"
-            ariaDescription="Czytaj więcej"
-            variant="secondary"
-          />
-        </Container>
-        <Container className="flex w-[320px] flex-col items-center gap-6 rounded bg-greenMain py-5 contrast:bg-yellowContrast tablet:w-[395px] tablet:flex-shrink-0 tablet:snap-start tablet:gap-3 tablet:px-6 desktop:snap-none">
-          <Image
-            src="/images/graves.png"
-            alt="Inwentarz grobów"
-            width={350}
-            height={190}
-            className="h-[191px] w-[288px] rounded object-cover tablet:mb-5 tablet:w-[350px]"
-          />
-          <div className="self-start">
-            <Heading color="white" contrast="black" variant="h3">
-              Inwentarz grobów
-            </Heading>
-          </div>
-          <p className="text-white contrast:text-black tablet:mb-1">
-            Dbamy o zachowanie pamięci o żołnierzach AK, dokumentując miejsca
-            ich spoczynku. Tworzymy szczegółowy rejestr grobów kombatantów,
-            umożliwiający ich odnalezienie i upamiętnienie.
-          </p>
-          <Button
-            label="Znajdź grób"
-            ariaDescription="Poznaj historię"
-            variant="secondary"
-          />
-        </Container>
+            największych podziemnych armii II wojny światowej."
+          buttonLabel="Poznaj historię"
+          image={{
+            src: "/images/history_AK.png",
+            alt: "Historia AK. Zdjęcie grupy żołnierzy: Grupa żołnierzy w mundurach polowych, ustawionych w szeregu. Żołnierze noszą zielone berety, a jeden z nich trzyma sztandar wojskowy i flagę. Tło to miejskie budynki.",
+            width: 350,
+            height: 190,
+          }}
+        />
+        <AboutUsLandingCard
+          heading="Szlak Partyzancki"
+          description="Jest propozycją dla Wszystkich rozmiłowanych w pieszych wędrówkach i historii oraz cenną pomocą dydaktyczną dla nauczycieli i rodziców w wychowaniu patriotycznym dzieci i młodzieży."
+          buttonLabel="Czytaj więcej"
+          image={{
+            src: "/images/partisan_trail.png",
+            alt: "Szlak Partyzancki. Zdjęcie mapy: Zbliżenie na starą, szczegółową mapę topograficzną z oznaczeniami rzek, dróg i granic.",
+            width: 350,
+            height: 190,
+          }}
+        />
+        <AboutUsLandingCard
+          heading="Inwentarz grobów"
+          description="Dbamy o zachowanie pamięci o żołnierzach AK, dokumentując miejsca ich spoczynku. Tworzymy szczegółowy rejestr grobów kombatantów, umożliwiający ich odnalezienie i upamiętnienie."
+          buttonLabel="Znajdź grób"
+          image={{
+            src: "/images/graves.png",
+            alt: "Inwentarz grobów. Zdjęcie krzyża pamiątkowego: Metalowy krzyż na pomniku w otoczeniu drzew. Krzyż zdobią polskie flagi, a w tle widać zalesiony krajobraz.",
+            width: 350,
+            height: 190,
+          }}
+        />
       </div>
     </section>
   );
---
+++ src/components/shared/AboutUsLandingCard/AboutUsLandingCard.test.tsx
@@ -0,0 +1,47 @@
+import { render, screen } from "@testing-library/react";
+import AboutUsLandingCard from "../AboutUsLandingCard/AboutUsLandingCard";
+import Image from "next/image";
+
+jest.mock("next/image", () => ({
+  __esModule: true,
+  default: ({ src, alt }: { src: string; alt: string }) => (
+    <Image src={src} alt={alt} />
+  ),
+}));
+
+describe("AboutUsLandingCard", () => {
+  const defaultProps = {
+    image: {
+      src: "/test-image.jpg",
+      alt: "Test Image",
+      width: 350,
+      height: 190,
+    },
+    heading: "Test Heading",
+    description: "This is a test description.",
+    buttonLabel: "Learn More",
+  };
+
+  it("renders the component with the correct content", () => {
+    render(<AboutUsLandingCard {...defaultProps} />);
+
+    // Verify image
+    const image = screen.getByAltText(defaultProps.image.alt);
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute("src", defaultProps.image.src);
+
+    // Verify heading
+    const heading = screen.getByText(defaultProps.heading);
+    expect(heading).toBeInTheDocument();
+
+    // Verify description
+    const description = screen.getByText(defaultProps.description);
+    expect(description).toBeInTheDocument();
+
+    // Verify button
+    const button = screen.getByRole("button", {
+      name: defaultProps.buttonLabel,
+    });
+    expect(button).toBeInTheDocument();
+  });
+});
