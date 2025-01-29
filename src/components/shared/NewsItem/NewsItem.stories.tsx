32eb9e2e76692a561524218ad4cd465b71651a12
PiotrDrechsler <pdrech@gmail.com>
2025-01-29 08:31:09 +0100

    fix
    storybook

--- src/components/shared/NewsItem/NewsItem.stories.tsx
+++ src/components/shared/NewsItem/NewsItem.stories.tsx
@@ -1,30 +1,13 @@
-import { Meta, StoryObj } from "@storybook/react/*";
+import { Meta, StoryObj } from "@storybook/react";
 import { NewsItem } from "./NewsItem";
 
 const meta: Meta<typeof NewsItem> = {
-  title: "Components/Shared/Heading(NewsItem)",
+  title: "Components/Shared/NewsItem",
   component: NewsItem,
   argTypes: {
-    heading: {
-      control: {
-        type: "text",
-      },
-    },
-    date: {
-      control: {
-        type: "text",
-      },
-    },
-
-    excerpt: {
-      control: {
-        type: "text",
-      },
-    },
-    id: {
-      control: {
-        type: "text",
-      },
+    post: {
+      control: "object",
+      description: "Obiekt zawierający dane artykułu",
     },
   },
 };
@@ -35,9 +18,15 @@ type Story = StoryObj<typeof NewsItem>;
 
 export const Default: Story = {
   args: {
-    heading: "Lorem ipsum",
-    date: "12.12.2024",
-    excerpt:
-      "Lorem ipsum dolor sit amet consectetur. Imperdiet est libero faucibus sed nullam nibh tempus massa ipsum. Vel commodo urna praesent justo convallis vel duis justo. Placerat ut egestas volutpat tincidunt sed. Diam pharetra sed ultricies mi eget mauris pharetra et ultrices. Eget nunc scelerisque viverra ",
+    post: {
+      id: "1",
+      title: "Lorem ipsum",
+      date: "12.12.2024",
+      excerpt:
+        "Lorem ipsum dolor sit amet consectetur. Imperdiet est libero faucibus sed nullam nibh tempus massa ipsum. Vel commodo urna praesent justo convallis vel duis justo. Placerat ut egestas volutpat tincidunt sed. Diam pharetra sed ultricies mi eget mauris pharetra et ultrices. Eget nunc scelerisque viverra.",
+      content:
+        "Lorem ipsum dolor sit amet consectetur. Imperdiet est libero faucibus sed nullam nibh tempus massa ipsum. Vel commodo urna praesent justo convallis vel duis justo. Placerat ut egestas volutpat tincidunt sed. Diam pharetra sed ultricies mi eget mauris pharetra et ultrices. Eget nunc scelerisque viverra.",
+      featured_media: "path/to/image.jpg",
+    },
   },
 };
