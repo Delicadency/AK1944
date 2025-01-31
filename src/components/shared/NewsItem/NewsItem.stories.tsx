b6ce4d89f0718c278e512542491631c6a182195b
PiotrDrechsler <pdrech@gmail.com>
2025-01-31 20:22:43 +0100

    Update NewsItem.stories.tsx
    fix error

--- src/components/shared/NewsItem/NewsItem.stories.tsx
+++ src/components/shared/NewsItem/NewsItem.stories.tsx
@@ -19,14 +19,14 @@ type Story = StoryObj<typeof NewsItem>;
 export const Default: Story = {
   args: {
     post: {
-      id: "1",
+      id: 1,
       title: "Lorem ipsum",
       date: "12.12.2024",
       excerpt:
         "Lorem ipsum dolor sit amet consectetur. Imperdiet est libero faucibus sed nullam nibh tempus massa ipsum. Vel commodo urna praesent justo convallis vel duis justo. Placerat ut egestas volutpat tincidunt sed. Diam pharetra sed ultricies mi eget mauris pharetra et ultrices. Eget nunc scelerisque viverra.",
       content:
         "Lorem ipsum dolor sit amet consectetur. Imperdiet est libero faucibus sed nullam nibh tempus massa ipsum. Vel commodo urna praesent justo convallis vel duis justo. Placerat ut egestas volutpat tincidunt sed. Diam pharetra sed ultricies mi eget mauris pharetra et ultrices. Eget nunc scelerisque viverra.",
-      featured_media: "path/to/image.jpg",
+      featured_media: 31232132,
     },
   },
 };
