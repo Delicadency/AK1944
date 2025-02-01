fff59d695b7f3997b2ef27853dc8c317fbf359d4
Agata Kamińska <delicadency@gmail.com>
2025-02-01 19:32:36 +0100

    feat: added tests for HeaderMobile component; feat: added ARIA attributes

--- src/components/Header/HeaderMobile/__tests__/HeaderMobile.test.tsx
+++ src/components/Header/HeaderMobile/__tests__/HeaderMobile.test.tsx
@@ -1,6 +1,8 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { HeaderMobile } from "../HeaderMobile";
-// import HeaderMenuIcon from "@/icons/HeaderMenuIcon";
+import { axe, toHaveNoViolations } from "jest-axe";
+
+expect.extend(toHaveNoViolations);
 
 jest.mock("../../HeaderLogo", () => ({
   HeaderLogo: () => <div>Logo</div>,
@@ -24,6 +26,35 @@ describe("HeaderMobile", () => {
     render(<HeaderMobile />);
     expect(screen.getByText("Światowy Związek Żołnierzy AK")).toBeInTheDocument();
   });
+  
+  it("renders the text with correct classes", () => {
+    render(<HeaderMobile/>);
+    expect(screen.getByText("Światowy Związek Żołnierzy AK")).toHaveClass("h-8 w-[135px] text-center font-courier text-14 text-white contrast:text-black00")
+  })
 
+  it("renders the menu button", () => {
+    render(<HeaderMobile />);
+    const menuButton = screen.getByTestId("button");
+    expect(menuButton).toBeInTheDocument();
+  });
  
+  it("renders the menu with correct classes", () =>{
+    render(<HeaderMobile/>);
+    expect(screen.getByTestId("mobile-menu")).toHaveClass("fixed right-0 top-0 z-50 h-full w-full transform transition-transform duration-300 ease-in-out bg-greenB")
+  });
+
+  it("toggles the mobile menu when clicking the menu button", () => {
+    render(<HeaderMobile />);
+    const menuButton = screen.getByTestId("button");
+    expect(screen.queryByTestId("mobile-menu")).toHaveClass("translate-x-full");
+    fireEvent.click(menuButton);
+    expect(screen.getByTestId("mobile-menu")).not.toHaveClass("translate-x-full");
+    fireEvent.click(menuButton);
+    expect(screen.queryByTestId("mobile-menu")).toHaveClass("translate-x-full");
+  });
+  it("does not have any accessibility violations (axe)", async () => {
+    const { container } = render(<HeaderMobile />);
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
 });
