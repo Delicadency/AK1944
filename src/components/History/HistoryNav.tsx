8573a630439cfe24da796bb281bfb55568209c7f
Bartek <114334919+barteknr1@users.noreply.github.com>
2025-02-02 17:09:02 +0100

    Update history page -create mobile historyNav component

--- src/components/History/HistoryNav.tsx
+++ src/components/History/HistoryNav.tsx
@@ -1,27 +1,32 @@
 import Container from "../shared/Container";
+import IconArrowVertical from "@/icons/IconArrowVertical";
 
 export default function HistoryNav() {
   return (
-    <Container className="mx-9">
-      <nav className="flex">
-        <ul className="ml-5">
-          <li>
-            <a href="#">5.Pułk Strzelców Konnych</a>
-          </li>
-          <li>
-            <a href="#">Akcja Burza</a>
-          </li>
-          <li>
-            <a href="#">Obwód Dębica SZP-ZWZ-AK</a>
-          </li>
-          <li>
-            <a href="#">Literatura</a>
-          </li>
-          <li>
-            <a href="#">Biogramy</a>
-          </li>
-        </ul>
-      </nav>
+    <Container as="nav" className="flex justify-center">
+      <IconArrowVertical className="z-[-1] translate-x-10" />
+      <ul className="flex flex-col gap-10">
+        <li className="flex items-center gap-5">
+          <div className="h-[50px] w-[50px] rounded-full bg-yellowVintage" />
+          <a href="#">5.Pułk Strzelców Konnych</a>
+        </li>
+        <li className="flex items-center gap-5">
+          <div className="h-[50px] w-[50px] rounded-full bg-greenC" />
+          <a href="#">Akcja Burza</a>
+        </li>
+        <li className="flex items-center gap-5">
+          <div className="h-[50px] w-[50px] rounded-full bg-greenC" />
+          <a href="#">Obwód Dębica SZP-ZWZ-AK</a>
+        </li>
+        <li className="flex items-center gap-5">
+          <div className="h-[50px] w-[50px] rounded-full bg-greenC" />
+          <a href="#">Literatura</a>
+        </li>
+        <li className="mb-[60px] flex items-center gap-5">
+          <div className="h-[50px] w-[50px] rounded-full bg-greenC" />
+          <a href="#">Biogramy</a>
+        </li>
+      </ul>
     </Container>
   );
 }
