2504d2de7c7ebfe4d31f6be3d0f7007c64e7f011
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2025-02-05 18:35:28 +0100

    Added Todo for 8697uvgeq

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["ak1944.pl"],
+    domains: ["ak1944.pl"], // Todo: https://app.clickup.com/t/8697uvgeq
   },
 };
 
