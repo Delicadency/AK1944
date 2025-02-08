8990970e6337b524c3424daf201966a9341c193a
Agata Kamińska <delicadency@gmail.com>
2025-02-08 21:59:03 +0100

    feat: boardData fie created

---
+++ src/data/boardData.ts
@@ -0,0 +1,45 @@
+export const boardData = {
+  data: [
+    { subtitle: "Prezes Honorowy:", members: ["kpt. Edmund Książek"] },
+    { subtitle: "Prezes:", members: ["Maciej Małozięć*"] },
+    {
+      subtitle: "Skład zarządu:",
+      members: [
+        "Elżbieta Jarzębska* – wiceprezes",
+        "Tomasz Czapla* – wiceprezes",
+        "Małgorzata Grzybowska* – sekretarz",
+        "Artur Lis* – skarbnik",
+        "Stanisława Książek* – księgowa",
+        "Wiesława Pawlus",
+        "Bronisław Ćwik",
+        "Stanisław Garstka",
+      ],
+    },
+    {
+      subtitle: "Komisja Rewizyjna:",
+      members: [
+        "Andrzej Żylicz – przewodniczący KR",
+        "Tadeusz Garstka – członek KR",
+        "Stanisław Nylec* – członek KR",
+        "Grzegorz Kloc* – zastępca członka",
+        "Krystian Tumiński* – zastępca członka",
+      ],
+    },
+    {
+      subtitle: "Delegaci na Walny Zjazd Okręgu:",
+      members: [
+        "Bronisław Ćwik – delegat",
+        "Elżbieta Jarzębska* – delegat",
+        " Małgorzata Grzybowska* – zastępca delegata",
+        "Andrzej Lis* – zastępca delegata",
+      ],
+    },
+  ],
+  note: "* członkowie zwyczajni bez uprawnień kombatanckich",
+
+  announcement:
+    "Zgodnie z § 43 Statutu ŚZŻAK, Zarząd Koła zwołuje Walne Zebranie Członków Środowiska 5. PSK AK ŚZŻAK w Dębicy.",
+
+  electionInfo:
+    "Władze Koła wybrano w trakcie Walnego Zebrania dnia 30 marca 2019 roku.",
+};
