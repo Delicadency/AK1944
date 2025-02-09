14c5232156b795d90472b55ae7a057ba01fd19b8
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2025-02-09 19:16:52 +0100

    Reimported social media icons SVG & size prop

--- src/icons/IconX.tsx
+++ src/icons/IconX.tsx
@@ -1,26 +1,31 @@
 import { DefaultIconProps } from "@/types";
 
-export default function IconX({ className }: DefaultIconProps) {
+interface Props extends DefaultIconProps {
+  size?: number;
+}
+
+export default function IconX({ className, size = 32 }: Props) {
   return (
     <svg
       className={className}
-      width="20"
-      height="20"
-      viewBox="0 0 20 20"
+      width={size}
+      height={size}
+      viewBox="0 0 33 33"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
+      <rect x="0.5" width="32" height="32" rx="6.5641" fill="white" />
       <path
-        d="M16.4266 20.2198H3.78926C1.69543 20.2198 -0.00195312 18.5224 -0.00195312 16.4286V3.79121C-0.00195312 1.69739 1.69543 0 3.78926 0H16.4266C18.5204 0 20.2178 1.69739 20.2178 3.79121V16.4286C20.2178 18.5224 18.5204 20.2198 16.4266 20.2198ZM11.3875 9.16209L16.0317 4.18613H14.8469L10.8662 8.45124L7.73843 4.18613H3.78926L8.65464 10.8207L3.78926 16.0337H4.97401L9.17593 11.5316L12.4774 16.0337H16.4266L11.3875 9.16209ZM5.43777 4.84431H7.05794L14.7781 15.3754H13.1579L5.43777 4.84431Z"
-        fill="url(#paint0_linear_1963_21304)"
+        d="M26.7857 32.3516H6.56593C3.21582 32.3516 0.5 29.6358 0.5 26.2857V6.06593C0.5 2.71582 3.21582 0 6.56593 0H26.7857C30.1358 0 32.8516 2.71582 32.8516 6.06593V26.2857C32.8516 29.6358 30.1358 32.3516 26.7857 32.3516ZM18.7231 14.6593L26.1538 6.6978H24.2582L17.889 13.522L12.8846 6.6978H6.56593L14.3505 17.3132L6.56593 25.6538H8.46154L15.1846 18.4505L20.467 25.6538H26.7857L18.7231 14.6593ZM9.20355 7.7509H11.7958L24.1481 24.6007H21.5558L9.20355 7.7509Z"
+        fill="url(#paint0_linear_2034_5537)"
       />
       <defs>
         <linearGradient
-          id="paint0_linear_1963_21304"
-          x1="10.1079"
+          id="paint0_linear_2034_5537"
+          x1="16.6758"
           y1="0"
-          x2="10.1079"
-          y2="20.2198"
+          x2="16.6758"
+          y2="32.3516"
           gradientUnits="userSpaceOnUse"
         >
           <stop offset="0.0789996" stopColor="#515151" />
