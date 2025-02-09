3a14aab13b362400ca6b45876e3593c1819c3d36
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2025-02-10 00:59:53 +0100

    Refactor Footer, remove redundant styles and classes

--- src/components/Footer/FooterNavigation.tsx
+++ src/components/Footer/FooterNavigation.tsx
@@ -3,28 +3,26 @@ import { navigationData } from "../../data/footerNavigationData";
 
 export const FooterNavigation = () => {
   return (
-    <div className="mb-6 w-full lg:space-x-5">
-      <div className="flex flex-col gap-[20px] md:flex-row lg:gap-[32px]">
-        {navigationData.map((sectionData, index) => (
-          <ul
-            key={index}
-            className="mb-4 flex flex-col gap-[20px] space-y-4 md:mb-0 md:w-1/2 md:gap-[32px] lg:w-[100%] lg:flex-row lg:justify-start lg:space-x-6 lg:space-y-0"
-          >
-            {sectionData.links.map((link, linkIndex) => (
-              <li key={linkIndex}>
-                <Link
-                  href={link.href}
-                  aria-label={link.ariaLabel}
-                  title={link.title}
-                  className="text-16 text-base font-normal transition-opacity hover:opacity-80 md:text-18 lg:text-20"
-                >
-                  {link.label}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        ))}
-      </div>
-    </div>
+    <nav className="flex w-full flex-1 flex-col justify-center gap-5 md:flex-row md:items-center md:justify-around lg:gap-8">
+      {navigationData.map((sectionData, index) => (
+        <ul
+          key={index}
+          className="mb-4 flex flex-col gap-5 space-y-4 md:mb-0 md:gap-8 lg:flex-row lg:justify-start lg:space-x-6 lg:space-y-0"
+        >
+          {sectionData.links.map((link, linkIndex) => (
+            <li key={linkIndex} className="lg:text-center">
+              <Link
+                href={link.href}
+                aria-label={link.ariaLabel}
+                title={link.title}
+                className="text-16 text-base font-normal transition-opacity hover:opacity-80 md:text-18 lg:text-20"
+              >
+                {link.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      ))}
+    </nav>
   );
 };
--- src/components/Footer/FooterSocialMedia.tsx
+++
@@ -1,7 +0,0 @@
-import { SocialMediaTray } from "../shared/SocialMedia/SocialMediaTray";
-
-export const FooterSocialMedia = () => (
-  <div className="mt-2 hidden space-x-4 md:flex lg:mr-8 lg:space-x-4">
-    <SocialMediaTray />
-  </div>
-);
