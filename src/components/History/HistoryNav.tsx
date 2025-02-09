2f2d5c3c73d12fa14a7edcdc84bd7be706dbc958
Bartek <114334919+barteknr1@users.noreply.github.com>
2025-02-09 18:35:55 +0100

    Add paper background to biogram in history page -fixed few positioning bugs

--- src/components/History/HistoryNav.tsx
+++ src/components/History/HistoryNav.tsx
@@ -14,11 +14,11 @@ export default function HistoryNav() {
       {isMobile ? (
         <IconArrowVertical className="z-[-1] translate-x-10" />
       ) : (
-        <IconArrowHorizontal className="z-[-1]" />
+        <IconArrowHorizontal className="z-[-10]" />
       )}
-      <ul className="mb-[60px] flex -translate-y-10 flex-col gap-10 desktop:w-full desktop:flex-row desktop:justify-evenly desktop:gap-0 desktop:px-20">
+      <ul className="mb-[60px] flex flex-col gap-10 desktop:w-full desktop:-translate-y-10 desktop:flex-row desktop:justify-evenly desktop:gap-0 desktop:px-20">
         <li className="flex items-center gap-5 desktop:flex-col">
-          <div className="h-[50px] w-[50px] rounded-full bg-yellowVintage" />
+          <div className="h-[50px] w-[50px] rounded-full bg-yellowVintage"></div>
           <a href="#">5.Pułk Strzelców Konnych</a>
         </li>
         <li className="flex items-center gap-5 desktop:flex-col">
