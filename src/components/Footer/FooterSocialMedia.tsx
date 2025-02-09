8b685cb272a0d8ea2eb363e59fbc24c94ac11d8b
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2025-02-09 21:42:03 +0100

    Use SocialMediaTray in Footer

--- src/components/Footer/FooterSocialMedia.tsx
+++ src/components/Footer/FooterSocialMedia.tsx
@@ -1,33 +1,7 @@
-import { SocialMediaIcon } from "../shared/SocialMediaIcon/SocialMediaIcon";
-
-// Todo: Hrefs in Constants or some Config?
-const socials = [
-  {
-    name: "Facebook",
-    src: "/images/IconFecebook.png",
-    href: "https://www.facebook.com/Szlak.Partyzancki",
-  },
-  {
-    name: "Instagram",
-    src: "/images/IconInstagram.png",
-    href: "https://www.instagram.com", // Todo: Update.
-  },
-  {
-    name: "YouTube",
-    src: "/images/IconYouTube.png",
-    href: "https://www.youtube.com/@armiakrajowadebica3809",
-  },
-  // {
-  //   name: "X",
-  //   src: "/images/IconX.png", // Todo: Add icon file.
-  //   href: "https://x.com/szlak",
-  // },
-];
+import { SocialMediaTray } from "../shared/SocialMedia/SocialMediaTray";
 
 export const FooterSocialMedia = () => (
   <div className="mt-2 hidden space-x-4 md:flex lg:mr-8 lg:space-x-4">
-    {socials.map(({ name, src, href }) => (
-      <SocialMediaIcon key={name} name={name} src={src} href={href} />
-    ))}
+    <SocialMediaTray />
   </div>
 );
