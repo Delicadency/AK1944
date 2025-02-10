da0d4beaacf3abe0f1c2629e52bbb72a97ce3b94
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2025-02-10 01:10:56 +0100

    Remove redundant classes & simplify

--- src/components/Footer/FooterNavigation.tsx
+++ src/components/Footer/FooterNavigation.tsx
@@ -3,11 +3,11 @@ import { navigationData } from "../../data/footerNavigationData";
 
 export const FooterNavigation = () => {
   return (
-    <nav className="flex w-full flex-1 flex-col justify-center gap-5 md:flex-row md:items-center md:justify-around lg:gap-8">
+    <nav className="flex w-full flex-1 flex-col justify-center gap-8 md:flex-row md:items-center md:justify-around">
       {navigationData.map((sectionData, index) => (
         <ul
           key={index}
-          className="mb-4 flex flex-col gap-5 space-y-4 md:mb-0 md:gap-8 lg:flex-row lg:justify-start lg:space-x-6 lg:space-y-0"
+          className="flex flex-col gap-8 lg:flex-row lg:space-x-6"
         >
           {sectionData.links.map((link, linkIndex) => (
             <li key={linkIndex} className="lg:text-center">
@@ -15,7 +15,7 @@ export const FooterNavigation = () => {
                 href={link.href}
                 aria-label={link.ariaLabel}
                 title={link.title}
-                className="text-16 text-base font-normal transition-opacity hover:opacity-80 md:text-18 lg:text-20"
+                className="text-base font-normal transition-opacity hover:opacity-80 md:text-18 lg:text-20"
               >
                 {link.label}
               </Link>
