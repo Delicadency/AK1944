c4791c09b393b6ded7ca4889c3fcdd34f2e3051d
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2025-02-10 02:36:41 +0100

    Remove commented code

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,34 +1,3 @@
-// import { FooterNavigation } from "./FooterNavigation";
-// import LogoFooter from "./LogoFooter";
-// import { FooterSupport } from "./FooterSupport";
-// import { FooterLegalInfo } from "./FooterLegalInfo";
-// import FooterSocialMedia from "./FooterSocialMedia";
-// import Container from "../shared/Container";
-
-// export const Footer = () => {
-//   return (
-//     <footer className="w-full bg-[#0A290A] px-6 py-8 text-white">
-//       <Container>
-//         <div className="md:py-19 flex flex-col md:flex-row md:items-start md:justify-between">
-//           <div className="mb-4 flex justify-center">
-//             <LogoFooter />
-//           </div>
-//           <div className="flex flex-col md:ml-[76px] md:mt-[49px] md:w-2/3 md:flex-row md:justify-between lg:items-center lg:ml-6 lg:w-[100%] ">
-//             <FooterNavigation />
-//             <FooterSocialMedia />
-//           </div>
-//         </div>
-//         <div className="md:mt-[29px] md:flex md:flex-row">
-//           <FooterSupport />
-//         </div>
-//         {/* Separator */}
-//         <div className="my-4 h-[1px] w-full bg-white md:my-8" />
-//         <FooterLegalInfo />
-//       </Container>
-//     </footer>
-//   );
-// }
-
 import Image from "next/image";
 import Container from "@/components/shared/Container";
 import { SocialMediaTray } from "@/components/shared/SocialMedia/SocialMediaTray";
