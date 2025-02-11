75ead7926d78687b75cf890c29d379b679ce4330
Agata Kamińska <delicadency@gmail.com>
2025-02-11 13:06:18 +0100

    fix: removed unnecessary tags and simplyfied the code

--- src/app/zwiazek/zarzad/page.tsx
+++ src/app/zwiazek/zarzad/page.tsx
@@ -5,56 +5,51 @@ import Container from "@/components/shared/Container";
 export default function BoardPage() {
   return (
     <section className="flex flex-col justify-center">
-      <Container>
-        <section className="pt-5 tablet:pb-[64px] desktop:py-10">
+      <Container as="section" className="pt-5 tablet:pb-[64px] desktop:py-10">
           {/* todo: replace to a proper Breadcrumbs component - https://app.clickup.com/t/8697w2c4t */}
-          <h3 className="font-lora text-28 text-greenMain" aria-label="Ścieżka nawigacji">
+          <h3
+            className="font-lora text-28 text-greenMain"
+            aria-label="Ścieżka nawigacji"
+          >
             Breadcrumbs Placeholder
           </h3>
-        </section>
         <div className="flex justify-center py-10 tablet:hidden">
           <LogoAK className="h-[64px] w-[176px]" />
         </div>
       </Container>
-      <div className="desktop:flex desktop:flex-col desktop:items-center desktop:justify-center">
-        <article
-          className="font-16 h-fit w-full bg-greenMain py-6 leading-6 text-backgroundMain desktop:min-h-[740px] desktop:w-[1022px]"
-        >
-          <Container>
-            <div className="mobile:hidden tablet:block">
-              <div className="flex flex-col items-center px-[88px] pt-3">
-                <LogoAK className="h-[100px] w-[275px]" />
-                <hr className="my-9 w-full" aria-hidden="true"></hr>
-              </div>
+
+      <Container
+        as="article"
+        className="font-16 h-fit w-full bg-greenMain py-6 leading-6 text-backgroundMain desktop:flex desktop:min-h-[740px] desktop:w-[1022px] desktop:flex-col desktop:justify-center"
+      >
+        <div className="mobile:hidden tablet:block">
+          <div className="flex flex-col items-center px-[88px] pt-3">
+            <LogoAK className="h-[100px] w-[275px]" />
+            <hr className="my-9 w-full" aria-hidden="true"></hr>
+          </div>
+        </div>
+        <div className="desktop:grid desktop:grid-cols-2">
+          {boardData.members.map((section, index) => (
+            <div key={index} className="tablet:px-[88px] desktop:pr-0">
+              <h4 className="font-bold" id={`section-${index}`}>
+                {section.title}
+              </h4>
+              <ul className="pb-6">
+                {section.names.map((name, i) => (
+                  <li key={i}>{name}</li>
+                ))}
+              </ul>
             </div>
-            <div className="desktop:grid desktop:grid-cols-2">
-              {boardData.members.map((section, index) => (
-                <div key={index} className="tablet:px-[88px] desktop:pr-0">
-                  <h4 className="font-bold" id={`section-${index}`}>
-                    {section.title}
-                  </h4>
-                  <ul className="pb-6">
-                    {section.names.map((name, i) => (
-                      <li key={i}>{name}</li>
-                    ))}
-                  </ul>
-                </div>
-              ))}
-            </div>
-            <p className="pb-6 tablet:px-[88px]">
-              {boardData.note}
-            </p>
-          </Container>
-        </article>
-        <Container>
-          <section
-            className="font-16 flex flex-col gap-10 pb-10 pt-6 leading-6 text-black tablet:px-6 desktop:mx-auto desktop:w-[1022px] desktop:px-0 desktop:pb-[150px]"
-          >
-            <p>{boardData.announcement}</p>
-            <p>{boardData.electionInfo}</p>
-          </section>
-        </Container>
-      </div>
+          ))}
+        </div>
+        <p className="pb-6 tablet:px-[88px]">{boardData.note}</p>
+        
+      </Container>
+
+      <Container as="section" className="font-16 flex flex-col gap-10 pb-10 pt-6 leading-6 text-black tablet:px-6 desktop:mx-auto desktop:w-[1022px] desktop:px-0 desktop:pb-[150px]">
+          <p>{boardData.announcement}</p>
+          <p>{boardData.electionInfo}</p>
+      </Container>
     </section>
   );
 }
