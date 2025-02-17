3aac5d473f0e743b23ddf13475cfb5f4f4af8efc
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2025-02-17 01:14:47 +0100

    Remove unused Ref

--- src/components/Header/HeaderTabletAndDesktop/HeaderTabAndDesktop.tsx
+++ src/components/Header/HeaderTabletAndDesktop/HeaderTabAndDesktop.tsx
@@ -1,38 +1,32 @@
-import { useRef } from "react";
 import ContrastSwitcher from "../../Switcher/ContrastSwitcher";
 import { HeaderLogo } from "../HeaderLogo";
 import { Button } from "../../shared/Button/Button";
 import { firstNav, secondNav } from "@/data/headerData";
 import { HeaderNavigation } from "./HeaderNavigation";
 
-export const HeaderTabAndDesktop = () => {
-  /* Todo: What is that ref for? */
-  const menuRef = useRef<HTMLDivElement>(null);
+export const HeaderTabAndDesktop = () => (
+  <div className="flex items-center justify-between gap-2 py-5">
+    <HeaderLogo aria-label="Logo strony głównej" />
 
-  return (
-    <div ref={menuRef} className="flex items-center justify-between gap-2 py-5">
-      <HeaderLogo aria-label="Logo strony głównej" />
-
-      <div
-        className="flex flex-col gap-6"
-        aria-label="Sekcja głównych linków nawigacyjnych"
-      >
-        <HeaderNavigation navItems={firstNav} />
-        <HeaderNavigation navItems={secondNav} baseIndex={4} />
-      </div>
-
-      <div
-        className="flex flex-col-reverse gap-4 desktop:flex-row desktop:items-center desktop:gap-10"
-        aria-label="Przełącznik kontrastu i przycisku wsparcia"
-      >
-        <Button
-          label="Wesprzyj"
-          ariaDescription="Kliknij, aby wesprzeć fundację"
-          href="/wesprzyj"
-          aria-label="Kliknij, aby przejść do strony wsparcia"
-        />
-        <ContrastSwitcher aria-label="Przełącznik kontrastu" />
-      </div>
+    <div
+      className="flex flex-col gap-6"
+      aria-label="Sekcja głównych linków nawigacyjnych"
+    >
+      <HeaderNavigation navItems={firstNav} />
+      <HeaderNavigation navItems={secondNav} baseIndex={4} />
     </div>
-  );
-};
+
+    <div
+      className="flex flex-col-reverse gap-4 desktop:flex-row desktop:items-center desktop:gap-10"
+      aria-label="Przełącznik kontrastu i przycisku wsparcia"
+    >
+      <Button
+        label="Wesprzyj"
+        ariaDescription="Kliknij, aby wesprzeć fundację"
+        href="/wesprzyj"
+        aria-label="Kliknij, aby przejść do strony wsparcia"
+      />
+      <ContrastSwitcher aria-label="Przełącznik kontrastu" />
+    </div>
+  </div>
+);
