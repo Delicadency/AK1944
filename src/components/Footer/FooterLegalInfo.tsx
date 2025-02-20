327efdd178738155dacf1cf75d09a3b72d387ec1
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2025-02-20 05:21:27 +0100

    Added Volunteers page and Footer Link to it

--- src/components/Footer/FooterLegalInfo.tsx
+++ src/components/Footer/FooterLegalInfo.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export const FooterLegalInfo = () => {
   return (
-    <div className="mt-[30px] w-full text-center">
-      <p className="mb-3 flex justify-center gap-[20px]">
+    <div className="mt-8 flex w-full flex-col gap-5 text-center">
+      <p className="flex justify-center gap-5">
         <Link
           href="/polityka-prywatnosci"
           className="text-sm transition-opacity hover:opacity-80 md:text-base"
@@ -22,10 +22,17 @@ export const FooterLegalInfo = () => {
           Regulamin
         </Link>
       </p>
-      <p className="mt-[20px] text-16">© 2023 . All rights reserved.</p>
-      <p className="mt-[20px] text-16">
+
+      <p className="text-16">© 2023 . All rights reserved.</p>
+
+      <Link
+        href="/wolontariusze"
+        className="text-sm transition-opacity hover:opacity-80 md:text-base"
+        aria-label="Przejdź do strony Wolontariuszy"
+        title="Przejdź do strony Wolontariuszy"
+      >
         Strona powstała dzięki pracy wolontariuszy.
-      </p>
+      </Link>
     </div>
   );
 };
