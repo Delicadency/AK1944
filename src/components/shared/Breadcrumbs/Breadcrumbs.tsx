ff6f405dc23e0ab9c3934945fb3337bbc6e8f3e7
Okazaki92 <vincentslominski@gmail.com>
2025-02-23 14:08:19 +0100

    feat/Implement Breadcrumbs component with navigation and back home icon

--- src/components/shared/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/shared/Breadcrumbs/Breadcrumbs.tsx
@@ -1,11 +1,54 @@
-export const Breadcrumbs = () => {
+"use client";
+
+import { usePathname } from "next/navigation";
+import IconBackHome from "@/icons/IconBackHome";
+import clsx from "clsx";
+import Link from "next/link";
+
+interface BreadcrumbProps {
+  iconFill?: "green" | "white";
+}
+
+const Breadcrumbs = ({ iconFill = "green" }: BreadcrumbProps) => {
+  const iconFillColor = {
+    green: "fill-greenMain",
+    white: "fill-white",
+  };
+
+  const pathname = usePathname();
+  const pathSegments = pathname.split("/").filter(Boolean);
+
+  const generatePreviousPath = (index: number) =>
+    "/" + pathSegments.slice(0, index).join("/");
+
   return (
-    <h3
-      className="font-lora text-28 text-greenMain"
-      aria-label="Ścieżka nawigacji"
-    >
-      Breadcrumbs Placeholder
-    </h3>
+    <nav>
+      <ol className="flex flex-row items-center">
+        <li className="flex flex-row items-center justify-center">
+          <Link href="/">
+            <IconBackHome className={clsx(iconFillColor[iconFill])} />
+          </Link>
+          <span className="mx-2 text-greenMain">{">"}</span>
+        </li>
+        {pathSegments.map((_, index) => (
+          <li key={index} className="flex items-center">
+            <Link
+              href={generatePreviousPath(index)}
+              className="capitalize text-gray-700 hover:text-green-600"
+            >
+              {generatePreviousPath(index) === "" ||
+              generatePreviousPath(index) === "/"
+                ? "Światowy Związek Żołnierzy AK"
+                : pathSegments[index - 1]}
+            </Link>
+            <span className="mx-2">{">"}</span>
+          </li>
+        ))}
+      </ol>
+    </nav>
   );
 };
+
+export default Breadcrumbs;
+
 //todo: replace to a proper Breadcrumbs component - https://app.clickup.com/t/8697w2c4t
