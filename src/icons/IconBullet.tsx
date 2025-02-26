4fe41c6a027a6bde574573749c37c757ae927df7
Agata Kamińska <delicadency@gmail.com>
2025-02-26 17:52:10 +0100

    feat: IconBullet created

---
+++ src/icons/IconBullet.tsx
@@ -0,0 +1,17 @@
+import { DefaultIconProps } from "@/types";
+
+interface Props extends DefaultIconProps {
+  size?: number;
+}
+export default function IconBullet({ className, size }: Props) {
+  <svg
+    className={className}
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="12" cy="12" r="12" fill="#8F0000" />
+  </svg>;
+}
