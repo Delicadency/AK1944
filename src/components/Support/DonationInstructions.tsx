925e6d012b2f43195849e94532c3e090af67edc5
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2025-03-02 03:15:57 +0100

    Refactor TaxSection DonationInstructions

--- src/components/Support/DonationInstructions.tsx
+++ src/components/Support/DonationInstructions.tsx
@@ -1,38 +1,17 @@
-import { cn } from "@/utils";
-import { Heading } from "../shared/Heading/Heading";
-
-interface Props {
-  includeHeader?: boolean;
-}
-
-export const DonationInstructions = ({ includeHeader }: Props) => {
-  return (
-    <>
-      {includeHeader && (
-        <Heading variant="h4" color="green" contrast="black">
-          Jak przekazać 1,5%?
-        </Heading>
-      )}
-      <div
-        className={cn(
-          "text-greenMain max-md:max-w-full desktop:text-xl",
-          !includeHeader && "mt-3",
-        )}
-      >
-        <p>W swoim rocznym zeznaniu podatkowym (PIT)</p>
-        <ul
-          aria-label="Instrukcja przekazania 1,5% podatku"
-          className="list-disc pl-6"
-        >
-          <li>
-            wpisz numer KRS: <strong>0000113420</strong>
-          </li>
-          <li>
-            Podaj cel szczegółowy: <strong>Okręg Tarnów Koło Dębica</strong>
-          </li>
-          <li>Potwierdź swój wybór i złóż deklarację – to wszystko!</li>
-        </ul>
-      </div>
-    </>
-  );
-};
+export const DonationInstructions = () => (
+  <div className="text-greenMain max-md:max-w-full desktop:text-xl">
+    <p>W swoim rocznym zeznaniu podatkowym (PIT)</p>
+    <ul
+      aria-label="Instrukcja przekazania 1,5% podatku"
+      className="list-disc pl-6"
+    >
+      <li>
+        wpisz numer KRS: <strong>0000113420</strong>
+      </li>
+      <li>
+        Podaj cel szczegółowy: <strong>Okręg Tarnów Koło Dębica</strong>
+      </li>
+      <li>Potwierdź swój wybór i złóż deklarację – to wszystko!</li>
+    </ul>
+  </div>
+);
