01a43016e4ba3b6f374131b085eb35fab6d13a14
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2025-03-02 04:33:54 +0100

    Redesign to match branding

--- src/components/ComingSoon/ComingSoon.tsx
+++ src/components/ComingSoon/ComingSoon.tsx
@@ -3,32 +3,30 @@ import { SocialMediaTray } from "@/components/shared/SocialMedia/SocialMediaTray
 
 export const ComingSoon = () => (
   <section className="flex min-h-[70vh] items-center justify-center bg-white p-4">
-    <article className="w-full max-w-2xl rounded-sm bg-emerald-50 p-4 shadow-lg">
-      <header className="flex items-center justify-center p-2 text-emerald-700">
-        <h2 className="text-2xl font-bold">Strona w przygotowaniu!</h2>
+    <article className="w-full max-w-2xl rounded-sm bg-greenMain p-4 font-lora text-white shadow-lg">
+      <header className="flex items-center justify-center p-2">
+        <h2 className="text-2xl font-bold">Strona w przygotowaniu.</h2>
       </header>
 
-      <main className="flex flex-col gap-4 p-4 text-center">
-        <p className="text-xl text-emerald-700">
-          Nasi niesamowici wolontariusze ciężko pracują nad tą stroną w tym
-          momencie.
+      <main className="flex flex-col items-center gap-10 p-4 text-center text-xl">
+        <p>
+          Nasi wolontariusze intensywnie pracują nad jej rozbudową, <br />
+          aby dostarczyć wartościowe treści. <br />
+          Dziękujemy za cierpliwość i zapraszamy już wkrótce!
         </p>
 
-        <p className="rounded-lg bg-emerald-100 p-4 text-emerald-600">
-          Doceniamy Twoją cierpliwość! Ta strona będzie wkrótce dla Was dostępna
-          wraz z ekscytującą i niesamowitą treścią.
+        <p>
+          W międzyczasie zachęcamy do śledzenia naszych profili <br />w Social
+          Mediach, aby być na bieżąco z działalnością <br />
+          Światowego Związku Żołnierzy Armii Krajowej.
         </p>
-      </main>
 
-      <div className="border-t border-emerald-100 p-4">
-        <p className="mb-4 text-center text-emerald-600">
-          W międzyczasie możesz zajrzeć na nasze media społecznościowe:
-        </p>
+        <hr className="w-2/3 border-t" />
 
         <SocialMediaTray className="flex items-center justify-center gap-6" />
-      </div>
+      </main>
 
-      <footer className="flex flex-col items-center justify-center gap-3 p-5">
+      <footer className="mt-6 flex flex-col items-center justify-center gap-3 p-5">
         <Button
           label="Powrót do strony głównej"
           ariaDescription="Powrót do strony głównej"
