17a5de04704c6faff372d3497731e23fc0a22c91
Agata Kamińska <delicadency@gmail.com>
2025-03-02 12:06:01 +0100

    fix: switched a tags to Next Links

--- src/components/History/HistoryNav.tsx
+++ src/components/History/HistoryNav.tsx
@@ -4,6 +4,7 @@ import Container from "../shared/Container";
 import IconArrowVertical from "@/icons/IconArrowVertical";
 import IconArrowHorizontal from "@/icons/IconArrowHorizontal";
 import { Routes } from "@/routes";
+import Link from "next/link";
 
 export default function HistoryNav() {
   const isMobile = useMediaQuery("(max-width: 767px)");
@@ -20,23 +21,23 @@ export default function HistoryNav() {
       <ul className="mb-[60px] flex -translate-y-10 flex-col gap-10 desktop:w-full desktop:flex-row desktop:justify-evenly desktop:gap-0 desktop:px-20">
         <li className="flex items-center gap-5 desktop:flex-col">
           <div className="h-[50px] w-[50px] rounded-full bg-yellowVintage" />
-          <a href={Routes.REGIMENT}>5.Pułk Strzelców Konnych</a>
+          <Link href={Routes.REGIMENT}>5.Pułk Strzelców Konnych</Link>
         </li>
         <li className="flex items-center gap-5 desktop:flex-col">
           <div className="h-[50px] w-[50px] rounded-full bg-greenC" />
-          <a href={Routes.OPERATION}>Akcja „Burza”</a>
+          <Link href={Routes.OPERATION}>Akcja „Burza”</Link>
         </li>
         <li className="flex items-center gap-5 desktop:flex-col">
           <div className="h-[50px] w-[50px] rounded-full bg-greenC" />
-          <a href={Routes.DEBICA_DISTRICT}>Obwód Dębica SZP-ZWZ-AK</a>
+          <Link href={Routes.DEBICA_DISTRICT}>Obwód Dębica SZP-ZWZ-AK</Link>
         </li>
         <li className="flex items-center gap-5 desktop:flex-col">
           <div className="h-[50px] w-[50px] rounded-full bg-greenC" />
-          <a href={Routes.LITERATURE}>Literatura</a>
+          <Link href={Routes.LITERATURE}>Literatura</Link>
         </li>
         <li className="flex items-center gap-5 desktop:flex-col">
           <div className="h-[50px] w-[50px] rounded-full bg-greenC" />
-          <a href={Routes.BIOGRAMS}>Biogramy</a>
+          <Link href={Routes.BIOGRAMS}>Biogramy</Link>
         </li>
       </ul>
     </Container>
