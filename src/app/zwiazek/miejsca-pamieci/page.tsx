8dfe99413af929d1c2eb5f69806d582124a1e726
Agnieszka Iwan <117773931+AgnieszkaIwan@users.noreply.github.com>
2025-03-02 18:44:22 +0100

    Prettier settings adjusting

--- src/app/zwiazek/miejsca-pamieci/page.tsx
+++ src/app/zwiazek/miejsca-pamieci/page.tsx
@@ -1,20 +1,17 @@
-
 import { MemorialPlace } from "@/components/MemorialPlace/MemorialPlace";
 import { memorialPlaces } from "@/data/memorialPlacesData";
 import Container from "@/components/shared/Container";
 import { Breadcrumbs } from "@/components/shared/Breadcrumbs/Breadcrumbs";
 
-
 export default function MiejscaPamieci() {
   return (
-
-    <Container className="mx-auto max-w-7xl p-4 pt-12 pb-20">
+    <Container className="mx-auto max-w-7xl p-4 pb-20 pt-12">
       <Breadcrumbs />
-      <div className="space-y-10 mt-10 ">
+      <div className="mt-10 space-y-10">
         {memorialPlaces.map((miejsce) => (
           <MemorialPlace key={miejsce.id} place={miejsce} />
         ))}
       </div>
     </Container>
   );
-};
+}
--- src/components/MemorialPlace/MemorialPlace.tsx
+++ src/components/MemorialPlace/MemorialPlace.tsx
@@ -5,21 +5,17 @@ import { Heading } from "@/components/shared/Heading/Heading";
 import Image from "next/image";
 
 export const MemorialPlace = ({ place }: { place: PlaceDetails }) => (
-  <article className="border-b tablet:border-b-4 border-greenMain pb-8 font-lora w-full flex flex-col tablet:grid tablet:grid-cols-2 desktop:grid-cols-3 gap-6">
-    <div className="order-2 tablet:order-2 desktop:order-1 w-full tablet:flex tablet:flex-col tablet:items-end desktop:items-start desktop:col-start-1">
+  <article className="flex w-full flex-col gap-6 border-b border-greenMain pb-8 font-lora tablet:grid tablet:grid-cols-2 tablet:border-b-4 desktop:grid-cols-3">
+    <div className="order-2 w-full tablet:order-2 tablet:flex tablet:flex-col tablet:items-end desktop:order-1 desktop:col-start-1 desktop:items-start">
       <Image
         src={place.image}
         alt={place.name}
         width={288}
         height={191}
-        className="rounded-desktop object-cover 
-          w-full h-[191px] 
-          tablet:w-[200px] tablet:h-[200px] 
-          desktop:w-[250px] desktop:h-[250px] 
-          transition-all duration-300"
+        className="rounded-desktop size-[191px] w-full object-cover transition-all duration-300 tablet:size-[200px] desktop:size-[250px]"
       />
       {/* BUTTON */}
-      <div className="w-full flex justify-center desktop:hidden mt-4">
+      <div className="mt-4 flex w-full justify-center desktop:hidden">
         <Button
           label="Czytaj dalej"
           variant="secondary"
@@ -29,30 +25,34 @@ export const MemorialPlace = ({ place }: { place: PlaceDetails }) => (
         />
       </div>
     </div>
-    <div className="order-1 tablet:order-1 desktop:order-2 flex flex-col gap-4 text-center tablet:text-left desktop:col-span-2 desktop:text-left">
+    <div className="order-1 flex flex-col gap-4 text-center tablet:order-1 tablet:text-left desktop:order-2 desktop:col-span-2 desktop:text-left">
       <Heading
         variant="h2"
         contrast="black"
         color="green"
-        className="text-[1.5rem] tablet:text-[1.75rem] desktop:text-[2rem] font-bold">
+        className="text-[1.5rem] font-bold tablet:text-[1.75rem] desktop:text-[2rem]"
+      >
         {place.name}
       </Heading>
       <div className="desktop:grid desktop:grid-cols-2 desktop:gap-6">
         <div className="flex flex-col gap-2">
           <div className="flex items-center gap-2 text-greenB">
-            <IconMapPin name="pin" className="w-4 h-4" />
+            <IconMapPin name="pin" className="h-4 w-4" />
             <Heading
               variant="h4"
               className="text-[1.25rem] tablet:text-[1.75rem] desktop:text-[1.5rem]"
               contrast="black"
-              color="green" >
+              color="green"
+            >
               {place.location}
             </Heading>
           </div>
-          <p className="font-sourceSans text-xl text-black">{place.description}</p>
+          <p className="font-sourceSans text-xl text-black">
+            {place.description}
+          </p>
         </div>
         {/* BUTTON (DESKTOP ONLY) */}
-        <div className="hidden desktop:flex desktop:justify-center desktop:items-start mt-4">
+        <div className="mt-4 hidden desktop:flex desktop:items-start desktop:justify-center">
           <Button
             label="Czytaj dalej"
             variant="secondary"
@@ -60,7 +60,6 @@ export const MemorialPlace = ({ place }: { place: PlaceDetails }) => (
             ariaDescription={`Przejdź do szczegółów miejsca: ${place.name}`}
             href={place.link}
           />
-
         </div>
       </div>
     </div>
