dd02768d80c33200a94e796f7cfaaa043cd13656
Agnieszka Iwan <117773931+AgnieszkaIwan@users.noreply.github.com>
2025-03-09 20:52:57 +0100

    kontntak-mobile view

--- src/app/kontakt/page.tsx
+++ src/app/kontakt/page.tsx
@@ -7,6 +7,7 @@ import { IconMail } from "@/icons/IconMail";
 import { IconPhone } from "@/icons/IconPhone";
 import { IconMapPin } from "@/icons/IconMapPin";
 import IconDollar from "@/icons/IconDollar";
+import { TaxCard } from "@/components/shared/TaxCard/TaxCard";
 
 
 export default function ContactPage() {
@@ -19,72 +20,86 @@ export default function ContactPage() {
             Kontakt
           </Heading>
         </div>
-        <section aria-labelledby="organization-info" className="text-left  text-greenMain font-source-sans">
-          <p id="organization-info" className="mb-6 text-1.25 font-semibold ">
-            Światowy Związek Żołnierzy Armii Krajowej – Środowisko 5. Pułku Strzelców Konnych Armii Krajowej w Dębicy
-          </p>
-        </section>
-        <section aria-labelledby="contact-info" className="space-y-4 text-left  text-greenMain">
+        <div className="text-[1.125rem] mb-[2.5rem]">
+          <section aria-labelledby="organization-info" className="text-left text-greenMain font-source-sans">
+            <p id="organization-info" className="mb-6 font-semibold ">
+              Światowy Związek Żołnierzy Armii Krajowej – Środowisko 5. Pułku Strzelców Konnych Armii Krajowej w Dębicy
+            </p>
+          </section>
+          <section aria-labelledby="contact-info" className="space-y-4 text-left  text-greenMain">
 
-          {/* Adres */}
-          <div className="flex items-start gap-3 ">
-            <IconMapPin className="w-6 h-6" />
-            <div>
-              <p>Adres siedziby:</p>
-              <p className="font-medium">39-200 Dębica, ul. Rzeszowska 15</p>
+            {/* Adres */}
+            <div className="flex items-center gap-3 ">
+              <IconMapPin className="w-8 h-8" />
+              <div>
+                <p>Adres siedziby:</p>
+                <p className="font-medium">39-200 Dębica, ul. Rzeszowska 15</p>
+              </div>
             </div>
-          </div>
 
-          {/* E-mail */}
-          <div className="flex items-start gap-3 ">
-            <IconMail className="w-6 h-6" />
-            <div>
-              <p>E-mail:</p>
-              <a href="mailto:ak.debica@gmail.com" className="font-medium">
-                ak.debica@gmail.com
-              </a>
+            {/* E-mail */}
+            <div className="flex items-center gap-3 ">
+              <IconMail className="w-8 h-8" />
+              <div>
+                <p>E-mail:</p>
+                <a href="mailto:ak.debica@gmail.com" className="font-medium">
+                  ak.debica@gmail.com
+                </a>
+              </div>
             </div>
-          </div>
 
-          {/* Telefon */}
-          <div className="flex items-start gap-3">
-            <IconPhone className="w-6 h-6" />
-            <div>
-              <p>Telefon:</p>
-              <a href="tel:+48505248666" className="font-medium">
-                +48 505 248 666
-              </a>
+            {/* Telefon */}
+            <div className="flex items-center gap-3">
+              <IconPhone className="w-8 h-8" />
+              <div>
+                <p>Telefon:</p>
+                <a href="tel:+48505248666" className="font-medium">
+                  +48 505 248 666
+                </a>
+              </div>
             </div>
-          </div>
-        </section>
+          </section>
 
-        {/* Dane prawne */}
-        <section aria-labelledby="legal-info" className="mt-6 space-y-4 text-left">
+          <hr className="border-t-[1px] border-black my-6" />
 
-          {/* NIP */}
-          <div className="flex items-start gap-3">
-            <IconBriefcase name="briefcase" className="w-6 h-6" />
-            <div>
-              <p >NIP:</p>
-              <p className="font-medium">5261710045</p>
+          {/* Dane prawne */}
+          <section aria-labelledby="informacje prawne" className="space-y-4 text-left max-w-[260px]">
+
+            {/* NIP */}
+            <div className="flex gap-5 items-center">
+              <div className="w-6 flex-shrink-0">
+                <IconBriefcase name="briefcase" className="w-8 h-8 text-gray-600" />
+              </div>
+              <div>
+                <p >NIP:</p>
+                <p className="font-medium">5261710045</p>
+              </div>
             </div>
-          </div>
-          {/* KRS */}
-          <div className="flex items-start gap-3"></div>
-          <IconBook name="book" className="w-6 h-6" />
-          <div>
-            <p >KRS:</p>
-            <p className="font-medium">5261710045</p>
-          </div>
-          <div className="flex items-start gap-3">
-            <IconDollar name="dollar" className="w-6 h-6" />
-            <div>
-              <p >Nr konta bankowego:</p>
-              <p className="font-medium">41 1090 2590 0000 0001 5299 0666</p>
+            {/* KRS */}
+            <div className="flex items-center gap-5">
+              <div className="w-6 flex-shrink-0">
+                <IconBook name="book" className="w-8 h-8" />
+              </div>
+              <div>
+                <p >KRS:</p>
+                <p className="font-medium">5261710045</p>
+              </div>
             </div>
-          </div>
-        </section>
-
+            <div className="flex items-center gap-5">
+              <div className="w-6 flex-shrink-0">
+                <IconDollar name="dollar" className="w-8 h-8" />
+              </div>
+              <div className="flex flex-col flex-wrap break-words">
+                <p >Nr konta bankowego:</p>
+                <p className="font-medium">Santander Bank Polska</p>
+                <p className="font-medium">41 1090 2590 0000 0001 5299 0666</p>
+              </div>
+            </div>
+          </section>
+        </div>
+        <div className="mb-[2.5rem]">
+          <TaxCard />
+        </div>
       </Container>
     </div>
   )
