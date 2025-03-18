0e53ac3befdbc44c50608cd63182a8b5d01d6201
Agata Kamińska <delicadency@gmail.com>
2025-03-18 06:41:14 +0100

    chore: changed to arrow function

--- src/icons/IconBullet.tsx
+++ src/icons/IconBullet.tsx
@@ -4,17 +4,15 @@ interface Props extends DefaultIconProps {
   size?: number;
 }
 
-export default function IconBullet({ className, size = 24 }: Props) {
-  return (
-    <svg
-      className={className}
-      width={size}
-      height={size}
-      viewBox="0 0 24 24"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <circle cx="12" cy="12" r="12" fill="currentColor" />
-    </svg>
-  );
-}
+export const IconBullet = ({ className, size = 24 }: Props) => (
+  <svg
+    className={className}
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="12" cy="12" r="12" fill="currentColor" />
+  </svg>
+);
