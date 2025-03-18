93aefce8a8ff04bd6d556c3642a6ec10d7793642
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2025-03-18 02:21:19 +0100

    Initial page nav

--- src/app/partnerzy/page.tsx
+++ src/app/partnerzy/page.tsx
@@ -1,5 +1,28 @@
-import { ComingSoon } from "@/components/ComingSoon/ComingSoon";
+import { Breadcrumbs } from "@/components/shared/Breadcrumbs/Breadcrumbs";
+import Container from "@/components/shared/Container";
 
 export default function PartnersPage() {
-  return <ComingSoon />;
+  return (
+    <div className="bg-backgroundMain pb-20 text-greenMain">
+      <Container as="main" className="space-y-20">
+        <Navigation />
+      </Container>
+    </div>
+  );
 }
+
+const Navigation = () => (
+  <nav
+    aria-labelledby="partners-nav-heading"
+    className="space-y-5 desktop:space-y-10"
+  >
+    <Breadcrumbs />
+
+    <h2
+      id="partners-nav-heading"
+      className="mb-10 font-lora text-2xl font-bold"
+    >
+      Partnerzy
+    </h2>
+  </nav>
+);
