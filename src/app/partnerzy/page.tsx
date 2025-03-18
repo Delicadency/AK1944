1abfff076629452390b33b6fc5c9683a6cfb472c
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2025-03-18 03:51:45 +0100

    Initial BulletList component

--- src/app/partnerzy/page.tsx
+++ src/app/partnerzy/page.tsx
@@ -4,12 +4,22 @@ import { Partnership } from "./_components/Partnership";
 import { Thread } from "./_components/Thread";
 import { OurPartners } from "./_components/OurPartners";
 import { ThankYou } from "./_components/ThankYou";
+import { BulletList } from "./_components/BulletList/BulletList";
 
 export default function PartnersPage() {
   return (
     <div className="bg-backgroundMain pb-20 text-greenMain">
       <Container as="main" className="space-y-20">
         <Navigation />
+        <BulletList
+          points={[
+            "Lorem ipsum twoja stara",
+            "Lorem ipsum wasza stara",
+            "Lorem ipsum nie zduplikowana stara",
+            "Lorem ipsum unikalna stara z kluczem",
+            "Lorem ipsum unikalna stara z klapkiem",
+          ]}
+        />
         <Partnership />
         <Thread />
         <OurPartners />
