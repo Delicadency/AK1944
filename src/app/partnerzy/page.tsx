2a0eb1448ced0d80d1abdcf2442c1d558a7e3ad9
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2025-03-18 03:53:27 +0100

    Remove twoja stara

--- src/app/partnerzy/page.tsx
+++ src/app/partnerzy/page.tsx
@@ -6,20 +6,19 @@ import { OurPartners } from "./_components/OurPartners";
 import { ThankYou } from "./_components/ThankYou";
 import { BulletList } from "./_components/BulletList/BulletList";
 
+const points = [
+  "Wsparcie wydarzeń edukacyjnych i historycznych.",
+  "Patronaty medialne i promowanie inicjatyw związanych z historią AK.",
+  "Wspólne projekty badawcze i publikacje.",
+  "Wsparcie finansowe lub rzeczowe na cele upamiętnienia i edukacji.",
+];
+
 export default function PartnersPage() {
   return (
     <div className="bg-backgroundMain pb-20 text-greenMain">
       <Container as="main" className="space-y-20">
         <Navigation />
-        <BulletList
-          points={[
-            "Lorem ipsum twoja stara",
-            "Lorem ipsum wasza stara",
-            "Lorem ipsum nie zduplikowana stara",
-            "Lorem ipsum unikalna stara z kluczem",
-            "Lorem ipsum unikalna stara z klapkiem",
-          ]}
-        />
+        <BulletList points={points} />
         <Partnership />
         <Thread />
         <OurPartners />
