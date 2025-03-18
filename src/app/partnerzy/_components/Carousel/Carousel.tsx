69a2d51f6188468146152a0c9c8976aa504d15a7
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2025-03-18 05:59:13 +0100

    Carousel items with hoverable links

--- src/app/partnerzy/_components/Carousel/Carousel.tsx
+++ src/app/partnerzy/_components/Carousel/Carousel.tsx
@@ -4,11 +4,18 @@ import Image from "next/image";
 import useEmblaCarousel from "embla-carousel-react";
 import Autoplay from "embla-carousel-autoplay";
 
-interface Props {
-  items: string[]; // Todo: Add { id, name, href }, Maybe extract <CarouselItem /> ?
+interface Partner {
+  id: string;
+  name: string;
+  href: string;
+  image: string; // Optional?
 }
 
-// Todo: Adjust spacings for viewports.
+interface Props {
+  items: Partner[];
+}
+
+// Todo: Adjust spacings for viewports. Maybe extract <CarouselItem /> ?
 export const Carousel = ({ items }: Props) => {
   const [emblaRef] = useEmblaCarousel(
     { loop: true, dragFree: true, slidesToScroll: "auto", align: "start" },
@@ -19,18 +26,20 @@ export const Carousel = ({ items }: Props) => {
     <div className="overflow-hidden" ref={emblaRef}>
       <ul className="flex gap-4 tablet:gap-6 desktop:gap-10">
         <div id="spacer" className="w-4 tablet:w-6 desktop:w-10" />
-        {items.map((item) => (
+        {items.map(({ id, name, href }) => (
           <li
-            key={item}
-            className="min-w-0 flex-shrink-0 flex-grow-0"
-            title={item}
+            key={id}
+            className="min-w-0 flex-shrink-0 flex-grow-0 hover:brightness-75"
+            title={name}
           >
-            <Image
-              src="/images/placeholder_image.png"
-              alt={item}
-              width={304}
-              height={304}
-            />
+            <a href={href} target="_blank" rel="noopener noreferrer">
+              <Image
+                src="/images/placeholder_image.png"
+                alt={name}
+                width={304}
+                height={304}
+              />
+            </a>
           </li>
         ))}
       </ul>
