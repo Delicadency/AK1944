898bc3c1828aa2f35c9c1d28536cb1e8dcb0ed41
Agnieszka Iwan <117773931+AgnieszkaIwan@users.noreply.github.com>
2025-03-23 21:28:10 +0100

    fix

--- src/app/kontakt/components/ContactInfo.tsx
+++ src/app/kontakt/components/ContactInfo.tsx
@@ -6,44 +6,43 @@ import { IconMail } from "@/icons/IconMail";
 import { IconPhone } from "@/icons/IconPhone";
 
 export const ContactInfo = () => (
-  <section
-    aria-labelledby="contact-info"
-    className="font-source-sans mb-10 text-lg text-greenMain"
-  >
-    <h2 id="contact-info" className="sr-only">
-      Informacje kontaktowe
-    </h2>
+    <section
+        aria-labelledby="contact-info"
+        className="font-source-sans mb-10 text-lg text-greenMain"
+    >
+        <h2 id="contact-info" className="sr-only">
+            Informacje kontaktowe
+        </h2>
 
-    <InfoList>
-      <InfoItem
-        icon={<IconMapPin className="h-8 w-8" />}
-        label="Adres siedziby:"
-        content={AK_DATA.address}
-      />
+        <InfoList>
+            <InfoItem
+                icon={<IconMapPin className="h-8 w-8" />}
+                label="Adres siedziby:"
+                content={AK_DATA.address}
+            />
 
-      <InfoItem
-        icon={<IconMail className="h-8 w-8" />}
-        label="E-mail:"
-        content={
-          <a href={`mailto:${AK_DATA.email}`} className="font-medium">
-            {AK_DATA.email}
-          </a>
-        }
-      />
+            <InfoItem
+                icon={<IconMail className="h-8 w-8" />}
+                label="E-mail:"
+                content={
+                    <a href={`mailto:${AK_DATA.email}`} className="font-medium">
+                        {AK_DATA.email}
+                    </a>
+                }
+            />
 
-      <InfoItem
-        icon={<IconPhone className="h-8 w-8" />}
-        label="Telefon:"
-        content={
-          <a
-            href={`tel:${AK_DATA.phone.replace(/\s+/g, "")}`}
-            className="font-medium"
-          >
-            {AK_DATA.phone}
-          </a>
-        }
-      />
-    </InfoList>
-    <div className="mt-2 rounded-sm bg-red-500 p-4 text-center text-white"></div>
-  </section>
+            <InfoItem
+                icon={<IconPhone className="h-8 w-8" />}
+                label="Telefon:"
+                content={
+                    <a
+                        href={`tel:${AK_DATA.phone.replace(/\s+/g, "")}`}
+                        className="font-medium"
+                    >
+                        {AK_DATA.phone}
+                    </a>
+                }
+            />
+        </InfoList>
+    </section>
 );
