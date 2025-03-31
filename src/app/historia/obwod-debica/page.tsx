a055004b763ecb6d28e036e0c7a401abfc95523d
Bartek <114334919+barteknr1@users.noreply.github.com>
2025-03-31 22:47:58 +0200

    refactor/optimize Places list rendering and enhance accessibility

--- src/app/historia/obwod-debica/page.tsx
+++ src/app/historia/obwod-debica/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 import Container from "@/components/shared/Container";
 import { historyData } from "@/data/historyData";
 import { Heading } from "@/components/shared/Heading/Heading";
@@ -7,18 +6,10 @@ import { Beginnings } from "@/components/History/DebicaPage/Beginnings";
 import { Places } from "@/components/History/DebicaPage/Places";
 import { Readiness } from "@/components/History/DebicaPage/Readiness";
 import { Expansion } from "@/components/History/DebicaPage/Expansion";
-import { useMediaQuery } from "@/hooks/useMediaQuery";
 import { Breadcrumbs } from "@/components/shared/Breadcrumbs/Breadcrumbs";
-import { useDisclosure } from "@/hooks/useDisclosure";
 
 export default function DebicaDistrictPage() {
   const { title } = historyData.debica ?? {};
-  const { isOpen, toggle } = useDisclosure();
-
-  const handleToggle = async () => {
-    toggle();
-  };
-  const isMobile = useMediaQuery("(max-width: 768px)");
 
   return (
     <div className="contrast:bg-black">
@@ -29,9 +20,9 @@ export default function DebicaDistrictPage() {
         </Heading>
         <Fortress />
         <Beginnings />
-        <Places onToggle={handleToggle} />
-        {isMobile ? isOpen && <Readiness /> : <Readiness />}
-        {isMobile ? isOpen && <Expansion /> : <Expansion />}
+        <Places />
+        <Readiness />
+        <Expansion />
       </Container>
     </div>
   );
--- src/components/History/DebicaPage/Places.tsx
+++ src/components/History/DebicaPage/Places.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Heading } from "@/components/shared/Heading/Heading";
 import { historyData } from "@/data/historyData";
 import { Button } from "@/components/shared/Button/Button";
@@ -13,7 +14,7 @@ interface PlacesProps {
 
 export const Places: React.FC<PlacesProps> = ({ onToggle }) => {
   const { placesTitle, places, places1 } = historyData.debica ?? {};
-  const isMobile = useMediaQuery("(max-width: 768px)");
+  const isMobile = useMediaQuery("(max-width: 767px)");
 
   const [openDescription, setOpenDescription] = useState<{
     [key: string]: boolean;
@@ -25,6 +26,7 @@ export const Places: React.FC<PlacesProps> = ({ onToggle }) => {
       [id]: !prev[id],
     }));
   };
+
   const [readMore, setReadMore] = useState(false);
   const handleClick = () => {
     const newState = !readMore;
@@ -42,51 +44,49 @@ export const Places: React.FC<PlacesProps> = ({ onToggle }) => {
           <ul className="flex flex-col gap-6">
             {places.map((place) => (
               <li key={place.id}>
-                <div
-                  className="flex items-center gap-3"
+                <button
+                  type="button"
+                  className="flex items-center gap-3 desktop:cursor-default desktop:items-baseline"
                   onClick={() => isMobile && toggleDescription(place.id)}
+                  aria-label={`Rozwiń opis dla: ${place.name}`}
+                  aria-expanded={openDescription[place.id] ? "true" : "false"}
+                  aria-controls={`place-description-${place.id}`}
                 >
-                  <div className="h-4 w-4 rounded-full bg-redMain" />
-                  <p className="text-lg contrast:text-yellowContrast">
+                  <div className="h-4 w-4 flex-shrink-0 rounded-full bg-redMain" />
+                  <p className="text-start text-lg contrast:text-yellowContrast">
                     {place.name}
+                    {!isMobile && place.description}
                   </p>
-                  {isMobile && (
-                    <button
-                      type="button"
-                      className="ml-auto"
-                      aria-label="Rozwiń opis"
-                    >
-                      <IconChevronDown
-                        className={cn(
-                          "size-7 transition-transform",
-                          openDescription[place.id] ? "rotate-180" : "rotate-0",
-                        )}
-                      />
-                    </button>
-                  )}
-                </div>
+                  <IconChevronDown
+                    className={cn(
+                      "size-7 transition-transform tablet:hidden desktop:hidden",
+                      openDescription[place.id] ? "rotate-180" : "rotate-0",
+                    )}
+                    aria-hidden="true"
+                  />
+                </button>
                 <p
                   className={cn(
                     "mt-2 text-lg contrast:text-yellowContrast",
                     isMobile && !openDescription[place.id] ? "hidden" : "block",
                   )}
                 >
-                  {place.description}
+                  {isMobile && place.description}
                 </p>
               </li>
             ))}
           </ul>
           <p className="mt-6 text-lg contrast:text-yellowContrast">{places1}</p>
-        </div>
-        {isMobile && (
           <Button
             label={readMore ? "Zwiń tekst" : "Czytaj więcej"}
-            ariaDescription={readMore ? "Zwiń tekst" : "Czytaj więcej"}
             variant={"secondary"}
-            className="m-auto"
+            className="m-auto desktop:hidden"
             onClick={handleClick}
+            ariaDescription={readMore ? "Zwiń tekst" : "Czytaj więcej"}
+            aria-expanded={readMore ? "true" : "false"}
+            aria-controls="readMore-text"
           />
-        )}
+        </div>
         <div className="shrink-0">
           <Image
             src="/images/history/debica/people.webp"
