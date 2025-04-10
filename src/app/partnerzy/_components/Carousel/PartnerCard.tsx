18b207ef49c81c73e4eb896a21c7edf95a130e2b
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2025-04-10 19:18:07 +0200

    Display name if no partner logo

--- src/app/partnerzy/_components/Carousel/PartnerCard.tsx
+++ src/app/partnerzy/_components/Carousel/PartnerCard.tsx
@@ -6,19 +6,26 @@ interface Props {
   image?: string;
 }
 
-const DEFAULT_IMAGE = "/images/placeholder_image.png";
-
 export const PartnerCard = ({ name, href, image }: Props) => (
   <article className="flex h-full w-full items-center justify-center px-3 py-1 contrast:bg-white">
-    <a href={href} target="_blank" rel="noopener noreferrer">
-      <Image
-        className="flex max-h-80 max-w-80 items-center justify-center"
-        src={image ?? DEFAULT_IMAGE}
-        alt={name}
-        title={name}
-        width={304}
-        height={304}
-      />
+    <a
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="max-h-80 max-w-80"
+    >
+      {image ? (
+        <Image
+          className="max-h-80 max-w-80"
+          src={image}
+          alt={name}
+          title={name}
+          width={304}
+          height={304}
+        />
+      ) : (
+        <h5 className="text-center text-xl font-bold text-greenMain">{name}</h5>
+      )}
     </a>
   </article>
 );
