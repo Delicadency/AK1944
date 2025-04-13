b33a966831ea2cededd6c4b4e8959971f0624ba6
Agata Kamińska <delicadency@gmail.com>
2025-04-13 23:58:01 +0200

    feat: TrailPoints created

--- src/app/szlak-partyzancki/page.tsx
+++ src/app/szlak-partyzancki/page.tsx
@@ -9,6 +9,7 @@ import { Partners } from "./_components/Partners";
 import { Section } from "./_components/Section";
 import { TrailSlug, trails } from "./data/trails";
 import { TrailSwitcher } from "./_components/TrailSwitcher";
+import { TrailPoints } from "./_components/TrailPoints";
 
 export default function PartisanTrailPage() {
   const [activeTrail, setActiveTrail] = useState<TrailSlug>("second");
@@ -24,6 +25,7 @@ export default function PartisanTrailPage() {
           displayName={trailData.displayName}
         />
         <Map map={trailData.map} />
+        <TrailPoints activeTrail={activeTrail} />
         <div className="flex flex-col justify-between desktop:flex-row desktop:pb-20">
           <Section sectionName="Geneza" />
           <Section sectionName="Rajdy" />
