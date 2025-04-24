b459cc8d5c4846094d74bc5ea62b8210fe74e606
Bartek <114334919+barteknr1@users.noreply.github.com>
2025-04-24 13:13:42 +0200

    refactor/remove useMediaQuery from HistoryBiogram

--- src/components/History/HistoryBiogram.tsx
+++ src/components/History/HistoryBiogram.tsx
@@ -5,46 +5,41 @@ import { Heading } from "../shared/Heading/Heading";
 import { historyData } from "@/data/historyData";
 import Image from "next/image";
 import { Button } from "../shared/Button/Button";
-import { useMediaQuery } from "@/hooks/useMediaQuery";
 import { Routes } from "@/routes";
 
-export const HistoryBiogramComponent = () => {
-  const { title, content } = historyData.history.biograms;
-  const isSmallScreen = useMediaQuery("(max-width: 1279px)");
-  return (
-    <BackgroundImage
-      alt=""
-      src={
-        isSmallScreen
-          ? "/images/history/main/paper_mobile_bg.webp"
-          : "/images/history/main/paper_desktop_bg.webp"
-      }
-      width="768"
-      height="948"
-      className="absolute object-fill"
-      aria-hidden="true"
-    >
-      <Container className="relative py-10 contrast:bg-yellowContrast">
-        <Heading variant="h3" contrast="black" color="green">
-          {title}
-        </Heading>
-        <div className="desktop:flex">
-          <p className="my-6 font-courier text-lg text-greenMain">{content}</p>
-          <Image
-            src="/images/history/main/veteran.webp"
-            alt="Zdjęcie weterana Armii Krajowej"
-            width={500}
-            height={1920}
-            className="m-auto"
-          ></Image>
-        </div>
-        <Button
-          label="Sprawdź"
-          ariaDescription="Sprawdź"
-          className="m-auto mt-6"
-          href={Routes.BIOGRAMS}
-        />
-      </Container>
-    </BackgroundImage>
-  );
-};
+const { title, content } = historyData.history.biograms;
+
+export const HistoryBiogramComponent = () => (
+  <BackgroundImage
+    alt=""
+    src="/images/history/main/paper_desktop_bg.webp"
+    width="768"
+    height="948"
+    className="absolute object-fill"
+    aria-hidden="true"
+  >
+    <Container className="relative py-10 contrast:bg-yellowContrast">
+      <Heading variant="h3" contrast="black" color="green">
+        {title}
+      </Heading>
+      <div className="desktop:flex">
+        <p className="my-6 font-courier text-lg text-greenMain desktop:w-2/3">
+          {content}
+        </p>
+        <Image
+          src="/images/history/main/veteran.webp"
+          alt="Zdjęcie weterana Armii Krajowej"
+          width={290}
+          height={354}
+          className="m-auto w-[300px] max-w-[300px]"
+        ></Image>
+      </div>
+      <Button
+        label="Sprawdź"
+        ariaDescription="Sprawdź"
+        className="m-auto mt-6"
+        href={Routes.BIOGRAMS}
+      />
+    </Container>
+  </BackgroundImage>
+);
