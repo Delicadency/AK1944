3b388e2780950a8287598246bb100567b607f969
Agata Kamińska <delicadency@gmail.com>
2025-04-28 21:08:18 +0200

    feat: images.ts created - data to update when we get photos from Maciej

---
+++ src/app/szlak-partyzancki/geneza/data/images.ts
@@ -0,0 +1,44 @@
+export type Images = {
+  id: string;
+  src: string;
+  alt: string;
+  mobileOrder?: number;
+};
+
+// ToDo: images and alts to update - https://app.clickup.com/t/8698wcd0t
+export const sideImages: Images[] = [
+  {
+    id: "map",
+    src: "/images/trail-origin-image.jpg",
+    alt: "Zdjęcie mapy Szlaku Partyzanckiego",
+    mobileOrder: 1,
+  },
+  {
+    id: "placeholder-1",
+    src: "/images/placeholder_image.jpg",
+    alt: "Placeholder",
+    mobileOrder: 2,
+  },
+  {
+    id: "placeholder-2",
+    src: "/images/placeholder_image.jpg",
+    alt: "Placeholder",
+    mobileOrder: 3,
+  },
+  {
+    id: "placeholder-3",
+    src: "/images/placeholder_image.jpg",
+    alt: "Placeholder",
+    mobileOrder: 4,
+  },
+  {
+    id: "placeholder-4",
+    src: "/images/placeholder_image.jpg",
+    alt: "Placeholder",
+  },
+  {
+    id: "placeholder-5",
+    src: "/images/placeholder_image.jpg",
+    alt: "Placeholder",
+  },
+];
