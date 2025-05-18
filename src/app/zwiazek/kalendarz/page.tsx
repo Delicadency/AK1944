4263ce0647293804e70b8df89ee9a68f5a0fdce5
Vincent Słomiński <vincentslominski@gmail.com>
2025-05-18 20:37:55 +0200

    Refactor calendar components for improved layout and accessibility; update event data and localization

--- src/app/zwiazek/kalendarz/page.tsx
+++ src/app/zwiazek/kalendarz/page.tsx
@@ -4,7 +4,7 @@ import Container from "@/components/shared/Container";
 
 export default function CalendarPage() {
   return (
-    <Container className="flex flex-col items-center gap-5 p-0 tablet:gap-16 desktop:max-w-full desktop:p-0">
+    <Container className="flex flex-col items-center gap-14 p-0 tablet:gap-16 desktop:max-w-full desktop:p-0">
       <CalendarPageHeader />
       <CalendarMain />
     </Container>
