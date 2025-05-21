ec44cfadc2bd18223aba4a92f424bec908833168
Jakub Wąsik <eghizio@gmail.com>
2025-05-21 19:14:29 +0200

    Added partner links and one logo

--- src/data/partners.ts
+++ src/data/partners.ts
@@ -1,176 +1,171 @@
-// Todo: https://app.clickup.com/t/8698cqvh8
 export const partners = [
   {
     id: "1",
     name: "Let’s talk about IT",
-    href: "https://www.letstalkaboutit.pl/",
+    href: "https://www.letstalkaboutit.pl",
     image: "/images/partners/Let's talk about IT.png",
   },
   {
     id: "2",
     name: "Związek Strzelecki „Strzelec” Józefa Piłsudskiego Jednostka Strzelecka 2017 w Dębicy",
-    href: "#",
+    href: "https://www.facebook.com/strzelec.debica",
     image:
       "/images/partners/Związek Strzelecki „Strzelec” Józefa Piłsudskiego Jednostka Strzelecka 2017 w Dębicy.png",
   },
   {
     id: "3",
     name: "Światowy Związek Żołnierzy Armii Krajowej Zarząd Główny",
-    href: "#",
+    href: "https://www.armiakrajowa.org.pl",
     image:
       "/images/partners/Światowy Związek Żołnierzy Armii Krajowej Zarząd Główny.png",
   },
   {
     id: "4",
     name: "Światowy Związek Żołnierzy Armii Krajowej Okręg Tarnów",
-    href: "#",
+    href: "https://www.facebook.com/szzak.tarnow",
     image:
       "/images/partners/Światowy Związek Żołnierzy Armii Krajowej Okręg Tarnów.png",
-    // image: "/images/partners/ŚZŻAK Okręg Tarnów 1.png"
   },
   {
     id: "5",
     name: "Fundacja Polskiego Państwa Podziemnego",
-    href: "#",
+    href: "https://www.fundacja-ppp.pl",
     image: "/images/partners/Fundacja Polskiego Państwa Podziemnego 1.png",
   },
   {
     id: "6",
     name: "Lasy Państwowe Nadleśnictwo Dębica",
-    href: "#",
+    href: "https://debica.krakow.lasy.gov.pl",
     image: "/images/partners/Lasy Państwowe 1.png",
   },
   {
     id: "7",
     name: "Miasto Dębica",
-    href: "#",
+    href: "https://debica.pl",
     image: "/images/partners/Miasto Dębica 1.png",
   },
   {
     id: "8",
     name: "Powiat Dębicki",
-    href: "#",
+    href: "https://www.powiatdebicki.pl",
     image: "/images/partners/Powiat Dębicki 1.png",
   },
   {
     id: "9",
     name: "Gmina Dębica",
-    href: "#",
+    href: "https://ugdebica.pl",
     image: "/images/partners/Gmina Dębica 1.png",
-    // image: "/images/partners/gmina Dębica.png",
   },
   {
     id: "10",
     name: "Gmina Pilzno",
-    href: "#",
+    href: "https://gminapilzno.pl",
     image: "/images/partners/gmina Pilzno.png",
-    // image: "/images/partners/Gmina Pilzno 1.png",
   },
   {
     id: "11",
     name: "Gmina Żyraków",
-    href: "#",
+    href: "https://www.zyrakow.pl",
     image: "/images/partners/gmina Żyraków.png",
   },
   {
     id: "12",
     name: "33. Batalion Lekkiej Piechoty im. Ppłk. Adama Lazarowicza „Klamry” w Dębicy",
-    href: "#",
+    href: "https://www.wojsko-polskie.pl/3bot",
     image: "/images/partners/33BatalionLekkiejPiechoty 1.png",
   },
   {
     id: "13",
     name: "Związek Harcerstwa Polskiego Hufiec ZHP im. Dębickich Szarych Szeregów w Dębicy",
-    href: "#",
+    href: "https://www.debica.zhp.pl",
     image: "/images/partners/ZHP.png",
   },
   {
     id: "14",
     name: "Muzeum Regionalne w Dębicy",
-    href: "#",
+    href: "https://www.muzeumwdebicy.pl",
     image: "/images/partners/Muzeum Regionalne w Dębicy 1.png",
-    // image: "/images/partners/muzeum regionalne w Dębicy.png",
   },
   {
     id: "15",
     name: "Miejski Ośrodek Kultury w Dębicy",
-    href: "#",
+    href: "https://mokdebica.pl",
     image: "/images/partners/Miejski Ośrodek Kultury w Dębicy 1.png",
-    // image: "/images/partners/mok w Dębicy.png"
   },
   {
     id: "16",
     name: "Miejska i Powiatowa Biblioteka Publiczna w Dębicy",
-    href: "#",
+    href: "https://www.mbpdebica.eu",
     image:
       "/images/partners/Miejska i Powiatowa Biblioteka Publiczna w Dębicy 1.png",
   },
   {
     id: "17",
     name: "Miejski Ośrodek Pomocy Społecznej w Dębicy",
-    href: "#",
+    href: "https://mops-debica.pl",
     image: "/images/partners/MOPS Dębica.png",
   },
   {
     id: "18",
     name: "Parafia pw. Wniebowzięcia Najświętszej Marii Panny w Gumniskach",
-    href: "#",
+    href: "https://www.parafiagumniska.pl",
     image: "/images/partners/Parafia Gumniska_1 1.png",
   },
   {
     id: "19",
     name: "Parafia pw. Św. Jadwigi w Dębicy",
-    href: "#",
+    href: "https://jadwigadebica.pl",
     image: "/images/partners/Parafia pw. św. Jadwigi Dębica 1.png",
   },
   {
     id: "20",
     name: "Publiczna Szkoła Podstawowa w Gumniskach im. Majora Adama Lazarowicza",
-    href: "#",
+    href: "https://gumniska.ugdebica.pl",
     image: "/images/partners/Szkoła Podstawowa w Gumniskach 1.png",
   },
   {
     id: "21",
     name: "Publiczna Szkoła Podstawowa nr 3 im. Tadeusza Bora-Komorowskiego w Dębicy",
-    href: "#",
+    href: "https://sp3debica.pl/",
     image:
       "/images/partners/Publiczna Szkoła Podstawowa nr 3 im. Tadeusza Bora-Komorowskiego w Dębicy.png",
   },
   {
     id: "22",
     name: "Publiczna Szkoła Podstawowa nr 11 z Oddziałami Integracyjnymi im. 5 Pułku Strzelców Konnych Armii Krajowej w Dębicy",
-    href: "#",
+    href: "https://spi11debica.com",
+    image: "/images/partners/szkola-podstawowa-11-w-debicy.png",
   },
   {
     id: "23",
     name: "Szkoła Podstawowa nr 12 im. Armii Krajowej w Dębicy",
-    href: "#",
+    href: "https://sp12debica.edupage.org",
     image:
       "/images/partners/Szkoła Podstawowa nr 12 im. Armii Krajowej w Dębicy.png",
   },
   {
     id: "24",
     name: "Publiczna Szkoła Podstawowa im. Stanisława Mroczki „Miecza” w Górze Motycznej",
-    href: "#",
+    href: "https://szkolagoramotyczna.szkolnastrona.pl",
     image:
       "/images/partners/Publiczna Szkoła Podstawowa im. Stanisława Mroczki „Miecza” w Górze Motycznej.png",
   },
   {
     id: "25",
     name: "Publiczna Szkoła Podstawowa im. Rotmistrza Witolda Pileckiego w Zwierniku",
-    href: "#",
+    href: "https://zszwiernik.superszkolna.pl",
   },
   {
     id: "26",
     name: "Szkoła Podstawowa im. III Zgrupowania Armii Krajowej „Pocisk” w Dobrkowie",
-    href: "#",
+    href: "https://dobrkow.szkolnastrona.pl",
     image:
       "/images/partners/Szkoła Podstawowa im. III Zgrupowania Armii Krajowej „Pocisk” w Dobrkowie.png",
   },
   {
     id: "27",
     name: "Liceum Ogólnokształcące im. Sebastiana Petrycego w Pilźnie",
-    href: "#",
+    href: "https://petrycy.pl",
     image:
       "/images/partners/Liceum Ogólnokształcące im. Sebastiana Petrycego w Pilźnie.png",
   },
