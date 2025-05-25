d8b7d00a25f41df83d48ee499f0673575bf04e5e
Vincent Słomiński <vincentslominski@gmail.com>
2025-05-25 18:59:54 +0200

    8697trqbk-literatura

--- src/app/historia/literatura/page.tsx
+++ src/app/historia/literatura/page.tsx
@@ -1,5 +1,23 @@
-import { ComingSoon } from "@/components/ComingSoon/ComingSoon";
+import { List } from "@/app/historia/literatura/_components/List";
+import { Breadcrumbs } from "@/components/shared/Breadcrumbs/Breadcrumbs";
+import Container from "@/components/shared/Container";
+import { Heading } from "@/components/shared/Heading/Heading";
 
 export default function LiteraturePage() {
-  return <ComingSoon />;
+  return (
+    <div className="contrast:bg-black00">
+      <Container
+        as="main"
+        className="tablet:px-10 tablet:pb-10 desktop:px-24 desktop:pb-20"
+      >
+        <div className="mb-8 flex flex-col items-start gap-4">
+          <Breadcrumbs />
+          <Heading variant="h2" color="green" contrast="yellow" className="m-0">
+            Literatura
+          </Heading>
+        </div>
+        <List />
+      </Container>
+    </div>
+  );
 }
