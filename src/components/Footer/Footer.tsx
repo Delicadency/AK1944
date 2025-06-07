b97c651d5abcdd1ad1e3e300591f2bd495a86234
Agata Kamińska <delicadency@gmail.com>
2025-06-07 21:50:11 +0200

    fix: fixed Next Image attributes to get rid of the console warnings

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,7 +8,7 @@ import { FooterLegalInfo } from "./FooterLegalInfo";
 export const Footer = () => (
   <footer className="w-full bg-greenMain px-6 py-8 text-white contrast:bg-yellowContrast contrast:text-black00">
     <Container>
-      <div className="flex flex-col gap-8 md:grid md:grid-cols-[minmax(150px,200px)_1fr] md:gap-4 lg:grid lg:grid-cols-[200px_1fr] lg:items-start lg:gap-8">
+      <div className="flex flex-col gap-8 md:grid md:grid-cols-[minmax(150px,200px)_1fr] md:gap-4 desktop:grid desktop:grid-cols-[200px_1fr] desktop:items-start desktop:gap-8">
         <FooterColumn>
           <Logo />
           <SocialMediaTray className="flex items-center gap-4" />
@@ -38,7 +38,8 @@ const Logo = () => (
     src="/images/LogoFooterDesktop.png"
     alt="Związek AK Logo"
     width={149}
-    height={181}
+    height={182}
+    className="h-auto w-[149px]"
   />
 );
 
