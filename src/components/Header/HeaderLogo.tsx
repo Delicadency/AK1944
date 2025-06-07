37f5a849562843e02391edcc51ec235d4c3f7092
Agata Kamińska <delicadency@gmail.com>
2025-06-07 23:36:45 +0200

    fix: Logo update

--- src/components/Header/HeaderLogo.tsx
+++ src/components/Header/HeaderLogo.tsx
@@ -3,16 +3,17 @@ import Link from "next/link";
 import { Routes } from "@/routes";
 
 export const HeaderLogo = () => (
-  <div className="w-[33px] tablet:w-[78px] desktop:w-[82px]">
+  <div className="w-[54px] tablet:w-[80px] desktop:w-[180px]">
     <Link href={Routes.HOME}>
       <Image
-        src="/images/logo.png"
+        src="/images/Logo_SZZAK.png"
         alt="Logo"
         priority
-        width={82}
-        height={100}
-        sizes="(max-width: 767px) 33px, (max-width: 1279px) 78px, 82px"
+        width={179}
+        height={65}
+        sizes="(max-width: 767px) 54px, (max-width: 1279px) 80px, 179px"
         aria-label="Logo"
+        className="h-auto w-[179px]"
       />
     </Link>
   </div>
